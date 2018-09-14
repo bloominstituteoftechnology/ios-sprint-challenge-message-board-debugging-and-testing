7ac2657f83078ed2fa976e7985299a72dbb6070a
Samantha Gatt <smfgatt@gmail.com>
2018-09-14 11:26:53 -0400

    Creates unit tests for message and message thread models

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,40 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    var messageThreadController = MessageThreadController()
+    
+    override func setUp() {
+        messageThreadController = MessageThreadController()
+    }
+    
+    
+    // MARK: - MessageThread
+    
+    func testCreatingMessageThread() {
+        let title = "Hello"
+        let messageThread = MessageThread(title: title)
+        XCTAssertEqual(messageThread.title, title)
+        XCTAssertEqual(messageThread.messages, [])
+        XCTAssertNotNil(messageThread.identifier)
+    }
+    
+    func testCreatingMessage() {
+        let text = "Bonjour"
+        let sender = "Samantha"
+        let message = MessageThread.Message(text: text, sender: sender)
+        XCTAssertEqual(message.messageText, text)
+        XCTAssertEqual(message.sender, sender)
+    }
+    
+    func testCreatingMessageThreadWithMessages() {
+        let message1 = MessageThread.Message(text: "Message 1", sender: "Samantha")
+        let message2 = MessageThread.Message(text: "Message 2", sender: "Samantha")
+        let messageThread = MessageThread(title: "Message Thread", messages: [message1, message2])
+        XCTAssertEqual(messageThread.messages, [message1, message2])
+    }
+    
+    
+    // MARK: - MessageThreadController
+    
     
 }
