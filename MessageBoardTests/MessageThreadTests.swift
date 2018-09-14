f7607939ca1b99b68a05f880e4a8463445d7e3eb
Conner Alegre <cga0@pm.me>
2018-09-14 09:49:31 -0600

    update testAddingMessageToMessageThreads (name changed)

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,15 +10,21 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
-    func testAddingMessageThreadToMessageThreads() {
+    func testAddingMessageToMessageThreads() {
         let messageThreadController = MessageThreadController()
         let messageThreadCount = messageThreadController.messageThreads.count
-        
-        
+        let expectationForMessageThread = expectation(description: "Appended to thread successfully")
+
         messageThreadController.createMessageThread(with: "Test Message") {
-            NSLog("Message created successfully")
+            expectationForMessageThread.fulfill()
         }
         
-        XCTAssertNotEqual(messageThreadCount, messageThreadController.messageThreads.count)
+        waitForExpectations(timeout: 1) { (error) in
+            if let error = error {
+                XCTFail("Error with creating message thread \(error)")
+            } else {
+                XCTAssertNotEqual(messageThreadCount, messageThreadController.messageThreads.count)
+            }
+        }
     }
 }
