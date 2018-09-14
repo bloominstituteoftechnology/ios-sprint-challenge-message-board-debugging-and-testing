220517c5145792d059f7b1d328145787bd9cc568
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-14 13:34:30 -0400

    Tests app with Unit Testing

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,53 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    var messageThreadController = MessageThreadController()
     
+    override func setUp() {
+        messageThreadController = MessageThreadController()
+    }
+    
+    func testCreateThread() {
+        let expectations = expectation(description: "Create")
+        XCTAssertEqual(messageThreadController.messageThreads, [])
+        
+        messageThreadController.createMessageThread(with: "Testing") {
+            XCTAssertNotEqual(self.messageThreadController.messageThreads, [])
+            expectations.fulfill()
+        }
+        waitForExpectations(timeout: 50, handler: nil)
+    }
+    
+    func testFetchThread() {
+        let expectations = expectation(description: "Fetch")
+        XCTAssertEqual(messageThreadController.messageThreads, [])
+        messageThreadController.fetchMessageThreads {
+            XCTAssertNotEqual(self.messageThreadController.messageThreads, [])
+            expectations.fulfill()
+        }
+        waitForExpectations(timeout: 50, handler: nil)
+    }
+    
+    func testCreateMessage() {
+        let expectation1 = expectation(description: "Fetch")
+        let expectation2 = expectation(description: "CreateMessage")
+        var fetchedThreads: [MessageThread] = []
+        XCTAssertEqual(messageThreadController.messageThreads, [])
+        
+        messageThreadController.fetchMessageThreads {
+            XCTAssertNotEqual(self.messageThreadController.messageThreads, [])
+            fetchedThreads = self.messageThreadController.messageThreads
+            expectation1.fulfill()
+            
+            XCTAssertNotNil(fetchedThreads.first)
+            guard let messageThread = fetchedThreads.first else { return }
+            let messages = messageThread.messages
+            
+            self.messageThreadController.createMessage(in: messageThread, withText: "Test", sender: "Lisa") {
+                XCTAssertNotEqual(messageThread.messages, messages)
+                expectation2.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 50, handler: nil)
+    }
 }
