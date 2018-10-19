8144c4ba0c5a7cd3bf880a589ea1e5f4859cfb69
Daniela Parra <dyparra47@gmail.com>
2018-10-19 12:06:41 -0400

    Found bugs and wrote unit tests for them. All passed.

--- Message Board/Model/MessageThread.swift
+++ Message Board/Model/MessageThread.swift
@@ -25,11 +25,11 @@ class MessageThread: Codable, Equatable {
         
         let title = try container.decode(String.self, forKey: .title)
         let identifier = try container.decode(String.self, forKey: .identifier)
-        let messages = try container.decodeIfPresent([Message].self, forKey: .messages) ?? []
+        let messages = try container.decodeIfPresent([String : Message].self, forKey: .messages) ?? [:]
         
         self.title = title
         self.identifier = identifier
-        self.messages = messages
+        self.messages = Array(messages.values)
     }
 
     
