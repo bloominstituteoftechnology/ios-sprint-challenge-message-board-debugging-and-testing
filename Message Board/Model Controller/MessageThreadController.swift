b32238bcf8602e25ba4d7bfbbdebbba3756c3c0e
Sean Hendrix <hendoware@gmail.com>
2018-12-07 11:15:21 -0500

    change firebase url

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://messageboard-fb33c.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
