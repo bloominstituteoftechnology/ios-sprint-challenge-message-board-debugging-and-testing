418db77f29b5a59c303682b1140f1e2f691bcf5c
Sean Hendrix <hendoware@gmail.com>
2018-12-07 11:37:25 -0500

    add accessibilityidentifiers to viewdidload in messagedetailviewcontroller

--- Message Board/View Controllers/MessageDetailViewController.swift
+++ Message Board/View Controllers/MessageDetailViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class MessageDetailViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        senderNameTextField.accessibilityIdentifier = "MessageDetailViewController.NameTextField"
+        messageTextView.accessibilityIdentifier = "MessageDetailViewController.MessageTextView"
+    }
+    
     // MARK: - Actions
     
     @IBAction func sendMessage(_ sender: Any) {
@@ -20,6 +27,9 @@ class MessageDetailViewController: UIViewController {
         
         messageThreadController?.createMessage(in: messageThread, withText: messageText, sender: senderName, completion: {
             print("Message created!")
+            DispatchQueue.main.async {
+                self.navigationController?.popViewController(animated: true)
+            }
         })
     }
 
