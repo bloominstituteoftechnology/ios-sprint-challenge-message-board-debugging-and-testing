0e26845b9b522e87498cd4ec395712bed427695f
Sean Hendrix <hendoware@gmail.com>
2018-12-07 11:24:00 -0500

    messagethreadtests

---
+++ Message BoardUITests/MessageThreadTestPage.swift
@@ -0,0 +1,11 @@
+//
+//  MessageThreadTestPage.swift
+//  Message BoardUITests
+//
+//  Created by Sean Hendrix on 12/7/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,6 +10,23 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
+    var messageThreadController = MessageThreadController()
     
+    override func setUp() {
+        messageThreadController = MessageThreadController()
+    }
     
+    func testCreateNewThread() {
+        let string = "Test Thread"
+        let numberOfMessages = messageThreadController.messageThreads.count
+        messageThreadController.createMessageThread(with: string) {
+            XCTAssertEqual(self.messageThreadController.messageThreads.count, numberOfMessages + 2)
+        }
+    }
+    
+    func testFetchThreads() {
+        messageThreadController.fetchMessageThreads {
+            XCTAssertEqual(self.messageThreadController.messageThreads.count, 0)
+        }
+    }
 }
