0be103480fe4eb0926efd3ff774e7252fb84ca4f
Ben Hakes <bhhakes@gmail.com>
2019-02-01 11:59:09 -0600

    add sender name requirement

--- Message Board/View Controllers/MessageDetailViewController.swift
+++ Message Board/View Controllers/MessageDetailViewController.swift
@@ -18,6 +18,13 @@ class MessageDetailViewController: UIViewController {
             let messageText = messageTextView.text,
             let messageThread = messageThread else { return }
         
+        guard senderName != "" else {
+                let alert = UIAlertController(title: "Sender Name Empty", message: "Please put in name and try again.", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return }
+        
+        
         messageThreadController?.createMessage(in: messageThread, withText: messageText, sender: senderName, completion: {
             print("Message created!")
         })
