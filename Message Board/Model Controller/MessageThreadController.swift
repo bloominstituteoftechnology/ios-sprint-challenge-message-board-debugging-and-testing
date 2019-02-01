3f32209ebf8c5521d0e8339a6ecda3cabca6c0b1
Ben Hakes <bhhakes@gmail.com>
2019-02-01 10:08:33 -0600

    update firebase database

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://first-project-263b0.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
