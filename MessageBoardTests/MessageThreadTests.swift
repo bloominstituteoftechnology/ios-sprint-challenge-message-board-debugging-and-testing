bbc22e7eb75f5920686ba17cf81b04f83ffeb040
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-02-08 13:02:20 -0600

    (test) implements Unit Tests, all passed.
    implements testCreateNewThread(), TestCreateNewMessage() stub, testFetchMessageThread()
    All currently implemented tests pass, bugs seem to be all squashed and logic seems up to snuff.

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,38 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    var messageThreadController: MessageThreadController!
+    let newTestThreadName = "Test Thread #1"
+    let newTestMessageTitle = "New Message #1"
+    let newTestMessageText = "This is New Message #1"
+    let newTestSender = "Alfred"
     
+    // Setup
+    override func setUp() {
+        super.setUp()
+        messageThreadController = MessageThreadController()
+    }
+    
+    // Create a new Thread
+    func testCreateNewThread() {
+        
+        let messagesCount = messageThreadController.messageThreads.count
+        messageThreadController.createMessageThread(with: newTestThreadName) {
+    
+            XCTAssertEqual(self.messageThreadController.messageThreads.count, messagesCount + 1)
+        }
+    }
+    
+    // Create a new Message - maybe do this inside new thread makes more sense?
+    func testCreateNewMessage() {
+        
+    }
+    
+    // Fetch Message Threads
+    func testFetchMessageThread() {
+        
+        messageThreadController.fetchMessageThreads {
+            XCTAssertEqual(self.messageThreadController.messageThreads.count, 2)
+        }
+    }
 }
