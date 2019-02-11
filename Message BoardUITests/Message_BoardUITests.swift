8901529d989ccd97a475ed7a2161d73b40152a01
Lotanna <sugabelly@gmail.com>
2019-02-10 21:06:53 -0500

    recorded UI Test for Create Thread
    UI Test recording successful but UI test failed.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -26,7 +26,13 @@ class Message_BoardUITests: XCTestCase {
     
         func testSegueProblem() {
         
+            let emptyListTable = XCUIApplication().tables["Empty list"]
+            emptyListTable/*@START_MENU_TOKEN@*/.textFields["Create a new thread:"]/*[[".textFields[\"Create a new thread:\"]",".textFields[\"createthreadfield\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.tap()
+            emptyListTable.tap()
         
+            let cellCount = emptyListTable.cells.count
+            
+            XCTAssert(cellCount > 0)
     }
     
 }
