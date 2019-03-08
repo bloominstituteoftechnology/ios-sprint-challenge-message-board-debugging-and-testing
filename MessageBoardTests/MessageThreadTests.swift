ecad658527bc8db6d7c61b219ebb4a5761cab201
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-08 11:30:00 -0500

    create unit test that creates a message thread sucessfully

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,7 +10,18 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
-    
 
+    var messageThreadController: MessageThreadController!
     
+    override func setUp() {
+        messageThreadController = MessageThreadController()
+    }
+    
+    
+    func testCreateMessageThread() {
+        messageThreadController.createMessageThread(with: "Unit Test Thread") {
+            let messageThreads = self.messageThreadController.messageThreads
+            XCTAssert(messageThreads.count > 0, "Test sucessfully created and located atleast on message thread")
+        }
+    }
 }
