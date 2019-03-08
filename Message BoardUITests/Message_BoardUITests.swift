58dc1f2d806c5073d1ac96636868135df8b0497d
Paul Yi <yipaulc@gmail.com>
2019-03-08 11:07:51 -0800

    Attempt to fix third bug
    After identifying and fixing the issue where the Send button saves an entry in the detail view controller, I had an issue with my second test failing.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,14 +23,14 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
-    func testMessageThreadTVCTextField() {
+    func testMessageThreadTextField() {
         
         threadTextField.tap()
         threadTextField.typeText("New Thread\n")
         XCTAssertTrue(cellAt(0).exists)
     }
     
-    func testMessageThreadTVCPersistsBetweenViews() {
+    func testThreadCellStillExistsAfterTappingCellAndBack() {
         
         threadTextField.tap()
         threadTextField.typeText("New Thread\n")
@@ -39,21 +39,49 @@ class Message_BoardUITests: XCTestCase {
         XCTAssertTrue(cellAt(0).exists)
     }
     
-    // MARK: - Private
+    func testMessagePersistsAfterTappingSendButton() {
+        
+        threadTextField.tap()
+        threadTextField.typeText("New Thread\n")
+        cellAt(0).tap()
+        nextButton.tap()
+        messageTextField.tap()
+        messageTextField.typeText("Paul")
+        messageTextView.tap()
+        messageTextView.typeText("Test that text view entry saves")
+        nextButton.tap()
+        XCTAssertTrue(cellAt(0).exists)
+        backButton.tap()
+        XCTAssertTrue(cellAt(0).exists)
+    }
+    
+    // MARK: - Private function
     
     private func cellAt(_ index: Int) -> XCUIElement {
-        
-        return app.textFields["MessageThreadTVC.textField"]
+        let cell = app.cells.element(boundBy: index)
+        return cell
     }
+    
+    // MARK: - Variables
     
     private var backButton: XCUIElement {
         return app.navigationBars.element(boundBy: 0)
     }
     
-    // MARK: - Properties
+    private var messageTextView: XCUIElement {
+        return app.textViews["MessageDetailViewController.messageTextView"]
+    }
+    
+    private var messageTextField: XCUIElement {
+        return app.textFields["MessageDetailViewController.senderTextField"]
+    }
+    
+    private var nextButton: XCUIElement {
+        return app.navigationBars.buttons.element(boundBy: 1)
+    }
     
     private var threadTextField: XCUIElement {
-        return app.textFields["MessageThreadTVC.textField"]
+        return app.textFields["MessageThreadTableViewController.textField"]
     }
     
     var app: XCUIApplication!
