a54c7f492b28b634b0577edbb4c9e0a1089b13f7
Michael Redig <mredig@gmail.com>
2019-06-14 10:45:02 -0500

    tabified message thread

--- Message Board/Model/MessageThread.swift
+++ Message Board/Model/MessageThread.swift
@@ -10,45 +10,45 @@ import Foundation
 
 class MessageThread: Codable, Equatable {
 
-    let title: String
-    var messages: [MessageThread.Message]
-    let identifier: String
+	let title: String
+	var messages: [MessageThread.Message]
+	let identifier: String
 
-    init(title: String, messages: [MessageThread.Message] = [], identifier: String = UUID().uuidString) {
-        self.title = title
-        self.messages = messages
-        self.identifier = identifier
-    }
+	init(title: String, messages: [MessageThread.Message] = [], identifier: String = UUID().uuidString) {
+		self.title = title
+		self.messages = messages
+		self.identifier = identifier
+	}
 
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        let title = try container.decode(String.self, forKey: .title)
-        let identifier = try container.decode(String.self, forKey: .identifier)
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+
+		let title = try container.decode(String.self, forKey: .title)
+		let identifier = try container.decode(String.self, forKey: .identifier)
 		let messages = try container.decodeIfPresent([String: Message].self, forKey: .messages) ?? [:]
-        
-        self.title = title
-        self.identifier = identifier
-		self.messages = messages.values.sorted { $0.timestamp < $1.timestamp }
-    }
 
-    
-    struct Message: Codable, Equatable {
-        
-        let messageText: String
-        let sender: String
-        let timestamp: Date
-        
-        init(text: String, sender: String, timestamp: Date = Date()) {
-            self.messageText = text
-            self.sender = sender
-            self.timestamp = timestamp
-        }
-    }
-    
-    static func ==(lhs: MessageThread, rhs: MessageThread) -> Bool {
-        return lhs.title == rhs.title &&
-            lhs.identifier == rhs.identifier &&
-            lhs.messages == rhs.messages
-    }
+		self.title = title
+		self.identifier = identifier
+		self.messages = messages.values.sorted { $0.timestamp < $1.timestamp }
+	}
+
+
+	struct Message: Codable, Equatable {
+
+		let messageText: String
+		let sender: String
+		let timestamp: Date
+
+		init(text: String, sender: String, timestamp: Date = Date()) {
+			self.messageText = text
+			self.sender = sender
+			self.timestamp = timestamp
+		}
+	}
+	
+	static func ==(lhs: MessageThread, rhs: MessageThread) -> Bool {
+		return lhs.title == rhs.title &&
+			lhs.identifier == rhs.identifier &&
+			lhs.messages == rhs.messages
+	}
 }
