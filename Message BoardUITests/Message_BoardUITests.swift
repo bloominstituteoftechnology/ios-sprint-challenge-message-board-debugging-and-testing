b3066e5c1acb77e69b31920ae7ed9e93dc2bb14f
Michael Redig <mredig@gmail.com>
2019-06-14 12:14:41 -0500

    added uitest for thread create added uitest for message create

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -26,6 +26,8 @@ class Message_BoardUITests: XCTestCase {
 		return cell
 	}
 
+	var backButtonText = "λ Message Board"
+
 	func tapElement(element: XCUIElement) {
 		XCTAssertTrue(element.isHittable)
 		element.tap()
@@ -47,12 +49,44 @@ class Message_BoardUITests: XCTestCase {
 	func testViewMessageThread() {
 		tapElement(element: cellNewThread)
 		app/*@START_MENU_TOKEN@*/.staticTexts["Checking to make sure this works."]/*[[".cells.staticTexts[\"Checking to make sure this works.\"]",".staticTexts[\"Checking to make sure this works.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeDown()
-		let backButton = app.navigationBars["A New Thread"].buttons["λ Message Board"]
+		let backButton = app.navigationBars["A New Thread"].buttons[backButtonText]
 		tapElement(element: backButton)
 
 		tapElement(element: cellTestingAgain)
-		let backButton2 = app.navigationBars["Testing again"].buttons["λ Message Board"]
+		let backButton2 = app.navigationBars["Testing again"].buttons[backButtonText]
 		tapElement(element: backButton2)
+	}
+
+	func testCreateThread() {
+		let textField = app.textFields["Create a new thread:"]
+		tapElement(element: textField)
+
+		textField.typeText("UITest New Thread\n")
+
+		let newCell = app.staticTexts["UITest New Thread"]
+		tapElement(element: newCell)
+		let backButton = app.navigationBars["UITest New Thread"].buttons[backButtonText]
+		tapElement(element: backButton)
+
+	}
+
+	func testCreateNewMessage() {
+		let message = "It's working... It's working!"
+
+
+		tapElement(element: cellNewThread)
+		tapElement(element: app.buttons["Add"])
+		let nameField = app.textFields["MessageDetail.nameField"]
+		tapElement(element: nameField)
+		nameField.typeText("UITester Person")
+
+
+		let textField = app.textViews["MessageDetail.messageTextField"]
+		tapElement(element: textField)
+		textField.typeText(message)
+		tapElement(element: app.navigationBars["New Message"].buttons["Send"])
+
+		XCTAssertTrue(app.staticTexts[message].exists)
 	}
     
 }
