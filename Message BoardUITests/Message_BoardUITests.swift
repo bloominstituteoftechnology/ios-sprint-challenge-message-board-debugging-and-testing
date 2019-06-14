d3115b4909cd8da0e8c0bd42407f2fc1268d7fbe
Michael Redig <mredig@gmail.com>
2019-06-14 11:57:44 -0500

    ui test: view message thread

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -9,7 +9,28 @@
 import XCTest
 
 class Message_BoardUITests: XCTestCase {
-        
+
+	var app: XCUIApplication {
+		return XCUIApplication()
+	}
+
+	var cellNewThread: XCUIElement {
+		let cell = app.tables.staticTexts["A New Thread"]
+		XCTAssertTrue(cell.exists)
+		return cell
+	}
+
+	var cellTestingAgain: XCUIElement {
+		let cell = app.tables.staticTexts["Testing again"]
+		XCTAssertTrue(cell.exists)
+		return cell
+	}
+
+	func tapElement(element: XCUIElement) {
+		XCTAssertTrue(element.isHittable)
+		element.tap()
+	}
+
     override func setUp() {
         super.setUp()
 
@@ -22,5 +43,16 @@ class Message_BoardUITests: XCTestCase {
         app.launchArguments = ["UITesting"]
         app.launch()
     }
+
+	func testViewMessageThread() {
+		tapElement(element: cellNewThread)
+		app/*@START_MENU_TOKEN@*/.staticTexts["Checking to make sure this works."]/*[[".cells.staticTexts[\"Checking to make sure this works.\"]",".staticTexts[\"Checking to make sure this works.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeDown()
+		let backButton = app.navigationBars["A New Thread"].buttons["λ Message Board"]
+		tapElement(element: backButton)
+
+		tapElement(element: cellTestingAgain)
+		let backButton2 = app.navigationBars["Testing again"].buttons["λ Message Board"]
+		tapElement(element: backButton2)
+	}
     
 }
