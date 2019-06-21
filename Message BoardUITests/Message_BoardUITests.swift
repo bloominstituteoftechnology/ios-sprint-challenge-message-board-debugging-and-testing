47f49548a7ebd64990a9a94fb030f794ed9b11f2
Alex <alexcc17@gmail.com>
2019-06-21 13:52:01 -0400

    Final commit. MVP.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -10,10 +10,14 @@ import XCTest
 
 class Message_BoardUITests: XCTestCase {
     
+    // MARK: - properties
+    
     var app: XCUIApplication {
         return XCUIApplication()
     }
 
+    // MARK: - VC Lifecycle
+    
     override func setUp() {
         super.setUp()
 
@@ -27,7 +31,8 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
-
+    // MARK: - Tests
+    
     func testCreateThread() {
         let searchTF = app.textFields["Create a new thread:"]
         searchTF.tap()
@@ -36,9 +41,11 @@ class Message_BoardUITests: XCTestCase {
         app.keyboards.buttons["Return"].tap()
         
         let newThread = app.staticTexts["Testing123 Thread"]
+        XCTAssert(newThread.exists)
         newThread.tap()
         
         let backBtn = app.navigationBars.buttons.element(boundBy: 0)
+        XCTAssert(backBtn.exists)
         backBtn.tap()
     }
     
@@ -64,9 +71,11 @@ class Message_BoardUITests: XCTestCase {
         enterMsgTV.typeText("This is a test")
         
         let sendBtn = app.buttons["Send"]
+        XCTAssert(sendBtn.exists)
         sendBtn.tap()
         
         let newMsg = app.tables.cells.staticTexts["Alex"]
+        XCTAssert(newMsg.exists)
         newMsg.tap()
 
     }
