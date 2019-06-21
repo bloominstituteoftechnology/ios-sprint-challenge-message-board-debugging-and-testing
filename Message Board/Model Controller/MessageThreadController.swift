6c85c06adb114c08345769611f6153f5e2f62250
Hayden Hastings <haydenhastings32@gmail.com>
2019-06-21 09:22:18 -0600

    Located but and MARKed it. Added testMessageThread to messageThreadController and successfully loaded.

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -72,7 +72,7 @@ class MessageThreadController {
             self.messageThreads.append(thread)
             completion()
             
-        }
+        }  // MARK: - Bug 1. No resume( data task was stopped and needs to be resumed.
     }
     
     func createMessage(in messageThread: MessageThread, withText text: String, sender: String, completion: @escaping () -> Void) {
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://message-board-ea8e1.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,16 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    var messageThreadController: MessageThreadController?
     
+    override func setUp() {
+        messageThreadController = MessageThreadController()
+    }
+    
+    func testCreateMessage() {
+        messageThreadController?.createMessageThread(with: "Test Message") {
+            guard let messageThreads = self.messageThreadController?.messageThreads else { return }
+            XCTAssert(messageThreads.count > 0, "Test message Created.")
+        }
+    }
 }
