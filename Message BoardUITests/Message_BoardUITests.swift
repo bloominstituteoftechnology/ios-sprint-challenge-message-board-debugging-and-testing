1e267ae23d4fdbc8705d61c7a44980b836e9321d
Alex <alexcc17@gmail.com>
2019-06-21 13:35:21 -0400

    Add tests

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -9,7 +9,11 @@
 import XCTest
 
 class Message_BoardUITests: XCTestCase {
-        
+    
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
+
     override func setUp() {
         super.setUp()
 
@@ -23,4 +27,48 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
+
+    func testCreateThread() {
+        let searchTF = app.textFields["Create a new thread:"]
+        searchTF.tap()
+        
+        searchTF.typeText("Testing123 Thread")
+        app.keyboards.buttons["Return"].tap()
+        
+        let newThread = app.staticTexts["Testing123 Thread"]
+        newThread.tap()
+        
+        let backBtn = app.navigationBars.buttons.element(boundBy: 0)
+        backBtn.tap()
+    }
+    
+    func testCreateMessage() {
+        let searchTF = app.textFields["Create a new thread:"]
+        searchTF.tap()
+        
+        searchTF.typeText("Testing123 Thread")
+        app.keyboards.buttons["Return"].tap()
+        
+        let newThread = app.staticTexts["Testing123 Thread"]
+        newThread.tap()
+        
+        let composeBtn = app.buttons["Add"]
+        composeBtn.tap()
+        
+        let enterNameTF = app.textFields["Enter your name:"]
+        enterNameTF.tap()
+        enterNameTF.typeText("Alex")
+        
+        let enterMsgTV = app.textViews.element(boundBy: 0)
+        enterMsgTV.tap()
+        enterMsgTV.typeText("This is a test")
+        
+        let sendBtn = app.buttons["Send"]
+        sendBtn.tap()
+        
+        let newMsg = app.tables.cells.staticTexts["Alex"]
+        newMsg.tap()
+
+    }
+
 }
