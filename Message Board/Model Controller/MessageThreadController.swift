dd6c1517c9c6abf884fccf151f51258759d4ee68
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-21 08:05:09 -0700

    init

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://projects-779a2.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
