997a859bb032f564ca714c8340164e3b4506fbe9
John Pitts <jgpitts@gmail.com>
2019-06-29 09:48:29 -0400

    added the necessary UITesting to test all functionality of app in basically one test, but created two just have two. testing creating messages is in series so really all has to be done in one test. Bug solved = using \n to make sure textField gets 'entered'. Another bug solved = you can't just add tablesQuery = app.tables and app = XCUIApplication in teh setUp. apparently SetUp really is worthless so it makes sense we deleted the boilerplate in UITests each time.  Lastly, the UI record feature is SOO buggy you might as well just learn how to write test code and type it manually for all the debugging of RECORD you'll have to do anyway.  It's a cute feature, but mostly worthless

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -28,19 +28,71 @@ class Message_BoardUITests: XCTestCase {
         let createANewThreadTextField = XCUIApplication().tables.textFields["Create a new thread:"]
         
         createANewThreadTextField.tap()
-        createANewThreadTextField.typeText("Pebbles")
+        createANewThreadTextField.typeText("Pebbles\n")
+        
+    }
+    
+    
+    func testCreateMessage2ndAttempt() {
+        
+        // reinvents the wheel
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        // repeats the create new message thread test, but it's the only way
+        let createANewThreadTextField = XCUIApplication().tables.textFields["Create a new thread:"]
+        createANewThreadTextField.tap()
+        createANewThreadTextField.typeText("Pebbles\n")
+        
+        // taps on cell with "Pebbles" which should work bc we just created it
+        tablesQuery.children(matching: .cell).element(boundBy: 0).staticTexts["Pebbles"].tap()
+        
+        // tap plus button
+        app.navigationBars["Pebbles"].buttons["Add"].tap()
+        
+        // fill out two fields for a message; hit "Send"
+        app.textFields["Enter your name:"].tap()
+        app.otherElements.containing(.navigationBar, identifier:"New Message").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+        app.navigationBars["New Message"].buttons["Send"].tap()
+ 
+    }
+    
+    func testEverythingUI() {
+        
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        let createANewThreadTextField = tablesQuery.textFields["Create a new thread:"]
+        createANewThreadTextField.tap()
+        createANewThreadTextField.typeText("Pebbles\n")
+        
+        app.tables.cells["messageThread.cell"].tap()
+        
+        
+        let pebblesNavigationBar = app.navigationBars["Pebbles"]
+        pebblesNavigationBar.buttons["Add"].tap()
+        app.textFields["Enter your name:"].tap()
+        app.otherElements.containing(.navigationBar, identifier:"New Message").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+        app.navigationBars["New Message"].buttons["Send"].tap()
+        
+        let writingStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Writing"]/*[[".cells.staticTexts[\"Writing\"]",".staticTexts[\"Writing\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        writingStaticText.tap()
+        writingStaticText.tap()
+        writingStaticText.tap()
+        pebblesNavigationBar.buttons["λ Message Board"].tap()
+        createANewThreadTextField.tap()
+        
         
     }
     
     func testCreateMessage() {
         
+        let app = XCUIApplication()
         let createANewThreadTextField = XCUIApplication().tables.textFields["Create a new thread:"]
         
         createANewThreadTextField.tap()
         createANewThreadTextField.typeText("Pebbles")
-
-
- //       app.tables/*@START_MENU_TOKEN@*/.staticTexts["Pebbles"]/*[[".cells.staticTexts[\"Pebbles\"]",".staticTexts[\"Pebbles\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        //use of unresolved identifier 'app'.  WHAT THE FUCK???
+        app.tables/*@START_MENU_TOKEN@*/.cells.staticTexts["Pebbles"]/*[[".cells.staticTexts[\"Pebbles\"]",".staticTexts[\"Pebbles\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.tap()
 //        app.navigationBars["Pebbles"].buttons["Add"].tap()
 //        app.textFields["Enter your name:"].tap()
 //        app.otherElements.containing(.navigationBar, identifier:"New Message").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
