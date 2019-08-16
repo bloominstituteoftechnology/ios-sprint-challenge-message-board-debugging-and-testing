db997b82d29cecfabf15f0dc88436e7d35ea93d4
Cameron <Cameron.dunn2013@gmail.com>
2019-08-16 11:02:51 -0500

    Added setUp override function for cleaner code.

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,29 +10,31 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
-    let messageThreadsTableVC = MessageThreadsTableViewController()
-    
+    var messageThreadsTableVC : MessageThreadsTableViewController?
+    override func setUp() {
+        messageThreadsTableVC = MessageThreadsTableViewController()
+    }
     
     func testRetreivingMessages(){
-        messageThreadsTableVC.retrieveMessages {
-            XCTAssertGreaterThan(self.messageThreadsTableVC.tableView.numberOfRows(inSection: 0), 0)
+        messageThreadsTableVC!.retrieveMessages {
+            XCTAssertGreaterThan(self.messageThreadsTableVC!.tableView.numberOfRows(inSection: 0), 0)
         }
     }
     
     func testPostingMessage(){
-        let testCount = messageThreadsTableVC.tableView.numberOfRows(inSection: 0)
-        while(messageThreadsTableVC.isViewLoaded == false){
-            messageThreadsTableVC.threadTitleTextField.text = "Test Text"
-            messageThreadsTableVC.createThread(messageThreadsTableVC.threadTitleTextField)
-            XCTAssertGreaterThan(self.messageThreadsTableVC.tableView.numberOfRows(inSection: 0), testCount)
+        let testCount = messageThreadsTableVC!.tableView.numberOfRows(inSection: 0)
+        while(messageThreadsTableVC!.isViewLoaded == false){
+            messageThreadsTableVC!.threadTitleTextField.text = "Test Text"
+            messageThreadsTableVC!.createThread(messageThreadsTableVC!.threadTitleTextField)
+            XCTAssertGreaterThan(self.messageThreadsTableVC!.tableView.numberOfRows(inSection: 0), testCount)
         }
     }
     
     func testTypingMessage(){
         let testString = "Testing the ability to write in the text field"
-        while messageThreadsTableVC.isViewLoaded == true{
-            messageThreadsTableVC.threadTitleTextField.text = testString
-            XCTAssertEqual(messageThreadsTableVC.threadTitleTextField.text, testString)
+        while messageThreadsTableVC!.isViewLoaded == true{
+            messageThreadsTableVC!.threadTitleTextField.text = testString
+            XCTAssertEqual(messageThreadsTableVC!.threadTitleTextField.text, testString)
             break
         }
     }
