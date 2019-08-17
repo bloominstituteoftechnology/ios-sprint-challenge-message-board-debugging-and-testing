c5276dfc1336fe46f7c270cbb690d0fdc7085fb6
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-08-17 14:04:46 -0400

    Wrote UI Tests for creating a thread and creating  a message.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -9,7 +9,22 @@
 import XCTest
 
 class Message_BoardUITests: XCTestCase {
-        
+    private var app: XCUIApplication {
+        return XCUIApplication()
+    }
+    
+    private var createThread: XCUIElement {
+        return app.tables.textFields["Create a new thread:"]
+    }
+    
+    private var senderName: XCUIElement {
+        return app.textFields["MessageDetailViewController.NameTextField"]
+    }
+    
+    private var messageText: XCUIElement {
+        return app.textViews["MessageDetailViewController.MessageTextView"]
+    }
+    
     override func setUp() {
         super.setUp()
 
@@ -22,11 +37,34 @@ class Message_BoardUITests: XCTestCase {
         app.launchArguments = ["UITesting"]
         app.launch()
     }
-    // Add a test to fetch messages
-    func testFetchingMessageThreads() {
-        //
+    
+    func testCreatingThread() {
+        createThread.tap()
+        createThread.typeText("Another Test")
+        app.keyboards.buttons["Return"].tap()
+        XCTAssertTrue(app.tables.staticTexts["Another Test"].exists)
     }
     
-    // Add test to create a thread
-    
+    func testCreatingMessage() {
+        createThread.tap()
+        createThread.typeText("Another Test")
+        app.keyboards.buttons["Return"].tap()
+
+        let thread = app.tables.staticTexts["Another Test"]
+        XCTAssertTrue(thread.exists)
+
+        thread.tap()
+        app.navigationBars.buttons["Add"].tap()
+
+        senderName.tap()
+        senderName.typeText("Michael")
+
+        messageText.tap()
+        messageText.typeText("This is a test.")
+
+        app.navigationBars.buttons["Send"].tap()
+
+        let message = app.tables.staticTexts["This is a test."]
+        XCTAssertTrue(message.exists)
+    }
 }
