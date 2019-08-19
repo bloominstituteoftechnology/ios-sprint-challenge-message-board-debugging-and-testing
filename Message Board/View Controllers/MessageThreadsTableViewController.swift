dcc33ca9f8278d185f8161bd7bf4c916889027fa
Kat Milton <mltnkat@gmail.com>
2019-08-19 08:49:33 -0700

    able to upload new threads reliably now.

--- Message Board/View Controllers/MessageThreadsTableViewController.swift
+++ Message Board/View Controllers/MessageThreadsTableViewController.swift
@@ -34,6 +34,8 @@ class MessageThreadsTableViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         }
+        
+        
     }
     
     // MARK: - UITableViewDataSource
