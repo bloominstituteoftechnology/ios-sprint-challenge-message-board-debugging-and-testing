7faa926d39d9cc63e164bd8f0dda363eb374c2c5
Marlon <marmoneymar@icloud.com>
2019-09-13 15:25:16 -0400

    Fixes bug - Not decoding correctly again. Initdecoder needed refactoring
    See Bugs.md #4
    
    P.S. - Last commit, see Bugs.md #3

--- Message Board/Model/MessageThread.swift
+++ Message Board/Model/MessageThread.swift
@@ -26,11 +26,11 @@ class MessageThread: Codable, Equatable {
         
         let title = try container.decode(String.self, forKey: .title)
         let identifier = try container.decode(String.self, forKey: .identifier)
-        let messages = try container.decodeIfPresent([Message].self, forKey: .messages) ?? []
+		let messageDict = try container.decodeIfPresent([String: MessageThread.Message].self, forKey: .messages) ?? [:]
+		self.messages = Array(messageDict.values)
         
         self.title = title
         self.identifier = identifier
-        self.messages = messages
     }
 
     
