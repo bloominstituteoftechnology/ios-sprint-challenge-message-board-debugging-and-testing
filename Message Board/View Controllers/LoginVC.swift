6b942da40119e3acca70f07d8925686d673b01b6
Jeffrey <SantanaJny@gmail.com>
2019-09-13 21:56:05 -0400

    Creating new vc to add username

---
+++ Message Board/View Controllers/LoginVC.swift
@@ -0,0 +1,49 @@
+//
+//  LoginVC.swift
+//  Message Board
+//
+//  Created by Jeffrey Santana on 9/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+	//MARK: - IBOutlets
+	
+	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var loginBtn: UIButton!
+	
+	//MARK: - Properties
+	
+	let messageThreadController = MessageThreadController()
+	
+	//MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		usernameTextField.becomeFirstResponder()
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let threadsVC = segue.destination.childViewControllers.first as? MessageThreadsTableViewController {
+			threadsVC.messageThreadController = messageThreadController
+		}
+	}
+	
+	//MARK: - IBActions
+	
+	@IBAction func loginBtnTapped(_ sender: Any) {
+		guard let username = usernameTextField.optionalText, username.count >= 2 else { return }
+		
+		messageThreadController.username = username
+		
+		performSegue(withIdentifier: "MessageThreadsTableViewControllerSegue", sender: nil)
+	}
+	
+	//MARK: - Helpers
+	
+	
+}
