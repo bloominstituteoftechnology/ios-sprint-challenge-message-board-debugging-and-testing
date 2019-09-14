135c59abe155da1a06057492d9161e30a1d4cb71
Jeffrey <SantanaJny@gmail.com>
2019-09-13 20:37:08 -0400

    Adding ui test to run through all screens

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -12,6 +12,18 @@ import XCTest
 class Message_BoardUITests: XCTestCase {
     
     var app: XCUIApplication!
+	
+	var threadTableView: XCUIElement {
+		let tableView = app.tables["MessageThreadsTableViewController.tableView"]
+		XCTAssertTrue(tableView.exists)
+		return tableView
+	}
+	
+	var messageTableView: XCUIElement {
+		let tableView = app.tables["MessageThreadDetailTableViewController.tableView"]
+		XCTAssertTrue(tableView.exists)
+		return tableView
+	}
     
     override func setUp() {
         super.setUp()
@@ -24,5 +36,53 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
 	
+	func testThreadCellOpensThread() {
+		let cell = threadTableView.cells.firstMatch
+		
+		XCTAssertTrue(cell.exists)
+		XCTAssertEqual("A New Thread", cell.staticTexts.element.label)
+		cell.tap()
+		XCTAssertFalse(cell.exists)
+	}
 	
+	func testMessageCellOpensMessage() {
+		let cell = threadTableView.cells.firstMatch
+		cell.tap()
+		
+		let messageCell = messageTableView.cells.firstMatch
+		
+		XCTAssertTrue(messageCell.exists)
+		XCTAssertEqual("Checking to make sure this works.", messageCell.staticTexts.firstMatch.label)
+		
+		let addBtn = app.navigationBars.buttons["Add"]
+		addBtn.tap()
+		XCTAssertFalse(messageCell.exists)
+	}
+	
+	func testNewMessageCreated() {
+		let cell = threadTableView.cells.firstMatch
+		cell.tap()
+		
+		let addBtn = app.navigationBars.buttons["Add"]
+		addBtn.tap()
+		
+		let sender = "Testing test"
+		let message = "This is a test"
+		let textField = app.textFields.element
+		let textView = app.textViews.element
+		
+		textField.tap()
+		textField.typeText(sender)
+		textView.tap()
+		textView.typeText(message)
+		
+		XCTAssertEqual(sender, textField.value as? String)
+		XCTAssertEqual(message, textView.value as? String)
+		
+		let sendBtn = app.navigationBars.buttons["Send"]
+		XCTAssertTrue(sendBtn.exists)
+		sendBtn.tap()
+		
+		XCTAssertTrue(addBtn.isHittable)
+	}
 }
