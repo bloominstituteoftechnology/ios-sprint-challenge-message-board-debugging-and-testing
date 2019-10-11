5dcab85222ac41e050f2eaf05532ab70f10c7ba3
Joshua Sharp <sharp.josh@gmail.com>
2019-10-11 11:18:59 -0400

    changed FB DB URL to mine.

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://message-board-a4aad.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
