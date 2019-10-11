58f9904830f14bbdcef84c5922ad5a0923f6d54a
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-12 01:22:21 +0800

    Add Unit tests

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -33,7 +33,7 @@ class MessageThreadController {
             guard let data = data else { NSLog("No data returned from data task"); completion(); return }
             
             do {
-                self.messageThreads = try JSONDecoder().decode([String: MessageThread].self, from: data).map({$0.value})
+                self.messageThreads = try JSONDecoder().decode([String: MessageThread].self, from: data).map({ $0.value })
             } catch {
                 self.messageThreads = []
                 NSLog("Error decoding message threads from JSON data: \(error)")
---
+++ bugs.md
@@ -0,0 +1,9 @@
+#  Bugs
+1. MessageThreadController - Missing .resume()
+
+2. MessageThreadController - Decoding from array instead of dictionary
+
+3. MessageThreadDetailTableViewController - Misspelled "AddMessage" string in segue
+
+4. 
+
