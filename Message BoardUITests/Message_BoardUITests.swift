90cc72ff5af3ed5e2d86fb41f7fbee210f82fee0
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-08 13:01:28 -0600

    added Bugs md file,

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,4 +23,38 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
+    func testTextField() {
+        
+        mBTextField.tap()
+        
+    }
+    
+    func testAddNewMessage() {
+        let identifiers = segues(ofViewController: )
+    }
+    
+    func testExpectation() {
+        let didFinish = expectation(description: "didFinish")
+        var name = ""
+        let url = URL(string: "https://apple.com")!
+
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            didFinish.fulfill()
+            name = "Dave"
+        }.resume()
+
+        wait(for: [didFinish], timeout: 5) // blocking sync wait
+
+        // Assertion only happens after the time out, or web request completes
+        XCTAssertEqual("Dave", name)
+    }
+    
+    
+    private var mBTextField: XCUIElement {
+        return app.textFields["newThreadTextField"]
+    }
+    
+    private var addButton: XCUIElement {
+        return app.buttons["Add"]
+    }
 }
