b24c98a68ec3473327608086f7b7d94dd8774e46
John Kouris <jdkouris@gmail.com>
2019-11-23 09:10:04 -0800

    Updated the baseURL to point to my firebase database

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://messageboard-1552c.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
