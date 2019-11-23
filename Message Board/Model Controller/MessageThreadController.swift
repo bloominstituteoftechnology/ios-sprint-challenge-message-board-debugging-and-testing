08b49de94b11cf0a952f600ed1f41ae117fb961e
dillonp23 <dillonp.lambda@gmail.com>
2019-11-23 09:34:06 -0800

    Add Bugs.md file and change MessageThreadController base url to personal firebase database

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://message-board-sprint-cha-acda9.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
