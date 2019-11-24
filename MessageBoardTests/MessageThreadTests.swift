2d1b5e1bb1c7136ed65bdd0f169b6fb13b2f147e
dillonp23 <dillonp.lambda@gmail.com>
2019-11-24 13:47:12 -0800

    Add Unit tests to test fetching, adding new threads, and adding a new message to a given thread

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,84 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    func testFetchThreadsFromServer() {
+       
+        let expectation = self.expectation(description: "Fetching")
+        let messageThreadController = MessageThreadController()
+        
+        messageThreadController.fetchMessageThreads {
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertNotNil(messageThreadController.messageThreads)
+    }
+    
+    func testCreateNewThread() {
+        
+        let messageThreadController = MessageThreadController()
+        let expectation = self.expectation(description: "Creating and Fetching")
+        
+        messageThreadController.createMessageThread(with: "Test Create New Thread") {
+            messageThreadController.fetchMessageThreads {
+                expectation.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        var threadTitleCheck: Bool = false
+        
+        for thread in messageThreadController.messageThreads {
+            if thread.title == "Test Create New Thread" {
+                threadTitleCheck = true
+            }
+        }
+        XCTAssertTrue(threadTitleCheck)
+    }
+    
+    func testCreateNewMessageInThread() {
+        
+        let messageThreadController = MessageThreadController()
+        let fetchExpectation = self.expectation(description: "Fetching Threads")
+        
+        var thread: MessageThread?
+        
+        messageThreadController.fetchMessageThreads {
+            thread = messageThreadController.messageThreads[0]
+            fetchExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        let createExpectation = self.expectation(description: "Creating Message")
+        guard let messageThread = thread else { return }
+        
+        messageThreadController.createMessage(in: messageThread, withText: "TestCreateNewMessageInThread", sender: "XCTEST") {
+            createExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        var messages: [MessageThread.Message?] = []
+        
+        for thread in messageThreadController.messageThreads {
+            if thread == messageThread {
+                messages = thread.messages
+            }
+        }
+        
+        var messageWasAdded: Bool = false
+        
+        for message in messages {
+            if message?.messageText == "TestCreateNewMessageInThread" && message?.sender == "XCTEST" {
+                messageWasAdded = true
+            }
+        }
+        
+        XCTAssertTrue(messageWasAdded)
+        
+    }
     
 }
