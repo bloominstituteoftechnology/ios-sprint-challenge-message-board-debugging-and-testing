fc7587f2a1fafebab93854654170cdc180b5a8c4
Fabiola <saga.fabiola@gmail.com>
2019-11-25 16:23:56 -0600

    Added UI Tests to create thread and send message

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -10,17 +10,63 @@ import XCTest
 
 class Message_BoardUITests: XCTestCase {
     
-    var app: XCUIApplication!
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
     
+    private var createThread: XCUIElement {
+        return app.tables.textFields["Create a new thread:"]
+    }
+    
+    private var sender: XCUIElement {
+        return app.textFields["SenderNameTextField"]
+    }
+    
+    private var messageText: XCUIElement {
+        return app.textViews["MessageTextview"]
+    }
     override func setUp() {
         super.setUp()
         
         continueAfterFailure = false
-        app = XCUIApplication()
+        let app = XCUIApplication()
         
         // NOTE: Keep this setup as is for UI Testing
         app.launchArguments = ["UITesting"]
         app.launch()
+    }
+    
+    func testCreateThread() {
+        createThread.tap()
+        createThread.typeText("UI Testing")
+        app.keyboards.buttons["Return"].tap()
+        XCTAssertTrue(app.tables.staticTexts["UI Testing"].exists)
+    }
+    
+    
+    func testCreateMessage() {
+        
+        createThread.tap()
+        createThread.typeText("UI Testing")
+        app.keyboards.buttons["Return"].tap()
+        
+        let newThread = app.tables.staticTexts["UI Testing"]
+        XCTAssertTrue(newThread.exists)
+        newThread.tap()
+        app.navigationBars.buttons["Add"].tap()
+        
+        let nameField = app.textFields["Enter your name:"]
+        nameField.tap()
+        nameField.typeText("Test Name")
+        let messageTextView = app.textViews.element(boundBy: 0)
+        messageTextView.tap()
+        messageTextView.typeText("New Message Test")
+        let saveButton = app.buttons["Send"]
+        saveButton.tap()
+        XCTAssert(app.tables.cells.staticTexts["Test Name"].exists)
+        
+        let newMessage = app.tables.staticTexts["New Message Test"]
+        XCTAssertTrue(newMessage.exists)
     }
     
 }
