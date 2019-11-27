c50097316db53deec0d0b02f77ec7762f232736c
Robert Keffury <bobby.keffury@me.com>
2019-11-27 12:32:59 -0800

    Fixed messages decoding bug and popviewController bug.

--- Message Board/Model/MessageThread.swift
+++ Message Board/Model/MessageThread.swift
@@ -28,11 +28,11 @@ class MessageThread: Codable, Equatable {
         
         let title = try container.decode(String.self, forKey: .title)
         let identifier = try container.decode(String.self, forKey: .identifier)
-        let messages = try container.decodeIfPresent([Message].self, forKey: .messages) ?? []
+        let messagesDic = try container.decodeIfPresent([String: Message].self, forKey: .messages) ?? [:]
         
         self.title = title
         self.identifier = identifier
-        self.messages = messages
+        self.messages = Array(messagesDic.values)
     }
 
     
