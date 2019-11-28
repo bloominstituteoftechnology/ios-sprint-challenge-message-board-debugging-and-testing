6fc4f21da2e93633c461a1ff0971ddb478c4810d
dillonp23 <dillonp.lambda@gmail.com>
2019-11-27 23:41:01 -0800

    More UI tests for app

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -31,6 +31,107 @@ class Message_BoardUITests: XCTestCase {
         XCTAssert(app.tables.cells["MessageCell"].exists)
     }
     
+    func testCreateNewThreadButton() {
+          
+        let rightNavBarButton = app.navigationBars.children(matching: .button).firstMatch
+        XCTAssert(rightNavBarButton.exists)
+        rightNavBarButton.tap()
+        
+    }
     
+    func testCreateNewThreadAndSave() {
+          
+        let rightNavBarButton = app.navigationBars.children(matching: .button).firstMatch
+        XCTAssert(rightNavBarButton.exists)
+        rightNavBarButton.tap()
+        
+        let textField = app.textFields["NewMessageThreadTextField"]
+        textField.tap()
+        textField.typeText("XCTEST NEW MESSAGE THREAD")
+        
+        let saveButton = app.navigationBars.children(matching: .button).firstMatch
+        XCTAssert(saveButton.exists)
+        saveButton.tap()
+        
+    }
     
+    func testBackButtonInMessageThread() {
+        
+        XCTAssert(app.tables.cells["MessageThreadCell"].exists)
+        
+        app.tables.cells.element(boundBy: 1).tap()
+        
+        XCTAssert(app.tables.cells["MessageCell"].exists)
+        
+        let backButton = app.navigationBars.children(matching: .button).firstMatch
+        XCTAssert(backButton.exists)
+        backButton.tap()
+        
+        XCTAssert(app.tables.cells["MessageThreadCell"].exists)
+        
+    }
+    
+    func testCreateNewMessage() {
+        
+        XCTAssert(app.tables.cells["MessageThreadCell"].exists)
+        
+        app.tables.cells.element(boundBy: 1).tap()
+        
+        let addButton = app.navigationBars.children(matching: .button).element(boundBy: 1)
+        
+        XCTAssert(addButton.exists)
+        
+        addButton.tap()
+        
+        let nameTextField = app.textFields["SenderNameTextField"]
+        XCTAssert(nameTextField.exists)
+        nameTextField.tap()
+        nameTextField.typeText("XCTEST UI")
+        
+        let messageTextField = app.textViews["MessageTextView"]
+        XCTAssert(messageTextField.exists)
+        messageTextField.tap()
+        messageTextField.typeText("THIS IS A XCTEST UI MESSAGE")
+        
+        let sendButton = app.navigationBars.children(matching: .button).element(boundBy: 1)
+        XCTAssert(sendButton.exists)
+        sendButton.tap()
+        
+        let okButton = app.alerts.buttons.firstMatch
+        XCTAssert(okButton.exists)
+        okButton.tap()
+        
+    }
+    
+    func testCancelANewMessage() {
+        
+        XCTAssert(app.tables.cells["MessageThreadCell"].exists)
+        
+        app.tables.cells.element(boundBy: 1).tap()
+        
+        let addButton = app.navigationBars.children(matching: .button).element(boundBy: 1)
+        
+        XCTAssert(addButton.exists)
+        
+        addButton.tap()
+        
+        let backButton = app.navigationBars.buttons.element(boundBy: 0)
+        XCTAssert(backButton.exists)
+        backButton.tap()
+        
+    }
+    
+    func testCancelANewThread() {
+        
+        let rightNavBarButton = app.navigationBars.buttons.element(boundBy: 0)
+        XCTAssert(rightNavBarButton.exists)
+        rightNavBarButton.tap()
+        
+        let backButton = app.navigationBars.element(boundBy: 0)
+        XCTAssert(backButton.exists)
+        
+        backButton.tap()
+        
+        
+    }
 }
