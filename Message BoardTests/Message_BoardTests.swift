0a5bf74085eb11988c1215ec14fb84c87e992d54
Robert Keffury <bobby.keffury@me.com>
2019-12-03 09:47:32 -0800

    Attempted to add UITests and Unit Tests

---
+++ Message BoardTests/Message_BoardTests.swift
@@ -0,0 +1,19 @@
+//
+//  Message_BoardTests.swift
+//  Message BoardTests
+//
+//  Created by Bobby Keffury on 12/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Message_Board
+
+class Message_BoardTests: XCTestCase {
+
+    func testFetchThreads() {
+        
+    }
+    
+
+}
--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Message_Board
 
 class Message_BoardUITests: XCTestCase {
     
@@ -21,6 +22,20 @@ class Message_BoardUITests: XCTestCase {
         // NOTE: Keep this setup as is for UI Testing
         app.launchArguments = ["UITesting"]
         app.launch()
+    }
+    
+    func testNewMessage() {
+        
+        let app = XCUIApplication()
+
+        let helloThread = app.tables/*@START_MENU_TOKEN@*/.staticTexts["Hello"]/*[[".cells.staticTexts[\"Hello\"]",".staticTexts[\"Hello\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let newMessageButton = app.navigationBars["Hello"].buttons["Add"]
+        let nameTextField = XCUIApplication().textFields["Enter your name:"]
+        
+        helloThread.tap()
+        newMessageButton.tap()
+        XCTAssertEqual(nameTextField.label, "")
+        
     }
     
 }
