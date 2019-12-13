c2220536fab9dabe6e99bfdda4cbea5c90e82fc7
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-12-13 09:54:30 -0800

    iOS11 MVP

---
+++ Message BoardUITests/Message_BoardUITests 2.swift
@@ -0,0 +1,26 @@
+//
+//  Message_BoardUITests.swift
+//  Message BoardUITests
+//
+//  Created by Spencer Curtis on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class Message_BoardUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        app = XCUIApplication()
+        
+        // NOTE: Keep this setup as is for UI Testing
+        app.launchArguments = ["UITesting"]
+        app.launch()
+    }
+    
+}
