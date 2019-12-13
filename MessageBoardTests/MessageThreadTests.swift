5533290514950c2cca6b081b030de95428fb4701
Jon Bash <jonbash@protonmail.com>
2019-12-13 09:06:28 -0800

    reorganize and write testFetchFromServer()

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,25 +10,55 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
+    // MARK: - Setup
+    
     var threadController: MessageThreadController!
     
     override func setUp() {
-        self.threadController = MessageThreadController()
+        threadController = MessageThreadController()
     }
     
+    // MARK: - Tests
+    
     func testCreateThread() {
+        let threadTitle = createThreadWithRandomTitle()
+        
+        XCTAssertTrue(threadController.messageThreads.contains {
+            $0.title == threadTitle
+        })
+    }
+    
+    func testFetchFromServer() {
+        let threadTitle = createThreadWithRandomTitle()
+        let threadFetchDidFinish = expectation(
+            description: "Finished fetching threads.")
+        
+        // reset local thread array to fetch fresh from server
+        threadController = MessageThreadController()
+        
+        threadController.fetchMessageThreads {
+            threadFetchDidFinish.fulfill()
+        }
+        wait(for: [threadFetchDidFinish], timeout: 5)
+        
+        XCTAssertTrue(threadController.messageThreads.contains {
+            $0.title == threadTitle
+        })
+    }
+    
+    // MARK: - Helper Methods
+    
+    func createThreadWithRandomTitle() -> String {
         let randomNumber = Double.random(in: 0...1000)
         let threadTitle = "new thread title \(randomNumber)"
         
-        let threadCreationDidFinish = expectation(description: "Finished creating thread with title \(threadTitle)")
+        let threadCreationDidFinish = expectation(
+            description: "Finished creating thread with title \(threadTitle)")
         
         threadController.createMessageThread(with: threadTitle) {
             threadCreationDidFinish.fulfill()
         }
         wait(for: [threadCreationDidFinish], timeout: 5)
-        
-        XCTAssertTrue(threadController.messageThreads.contains {
-            $0.title == threadTitle
-        })
+        return threadTitle
     }
 }
