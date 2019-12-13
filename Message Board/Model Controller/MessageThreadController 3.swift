8c4a10501b12a436cc98c9375c0804e634b5e6f4
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-12-13 10:03:59 -0800

    MVP

---
+++ Message Board/Model Controller/MessageThreadController 3.swift
@@ -0,0 +1,116 @@
+//
+//  MessageThreadController.swift
+//  Message Board
+//
+//  Created by Spencer Curtis on 8/7/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class MessageThreadController {
+    
+    func fetchMessageThreads(completion: @escaping () -> Void) {
+        
+        let requestURL = MessageThreadController.baseURL.appendingPathExtension("json")
+        
+        // This if statement and the code inside it is used for UI Testing. Disregard this when debugging.
+        if isUITesting {
+            fetchLocalMessageThreads(completion: completion)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error fetching message threads: \(error)")
+                completion()
+                return
+            }
+            
+            guard let data = data else { NSLog("No data returned from data task"); completion(); return }
+            
+            do {
+                self.messageThreads = try JSONDecoder().decode([MessageThread].self, from: data)
+            } catch {
+                self.messageThreads = []
+                NSLog("Error decoding message threads from JSON data: \(error)")
+            }
+            
+            completion()
+        }.resume()
+    }
+    
+    func createMessageThread(with title: String, completion: @escaping () -> Void) {
+        
+        // This if statement and the code inside it is used for UI Testing. Disregard this when debugging.
+        if isUITesting {
+            createLocalMessageThread(with: title, completion: completion)
+            return
+        }
+        
+        let thread = MessageThread(title: title)
+        
+        let requestURL = MessageThreadController.baseURL.appendingPathComponent(thread.identifier).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(thread)
+        } catch {
+            NSLog("Error encoding thread to JSON: \(error)")
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error with message thread creation data task: \(error)")
+                completion()
+                return
+            }
+            
+            self.messageThreads.append(thread)
+            completion()
+            
+        }
+    }
+    
+    func createMessage(in messageThread: MessageThread, withText text: String, sender: String, completion: @escaping () -> Void) {
+        
+        // This if statement and the code inside it is used for UI Testing. Disregard this when debugging.
+        if isUITesting {
+            createLocalMessage(in: messageThread, withText: text, sender: sender, completion: completion)
+            return
+        }
+        
+        guard let index = messageThreads.index(of: messageThread) else { completion(); return }
+        
+        let message = MessageThread.Message(text: text, sender: sender)
+        messageThreads[index].messages.append(message)
+        
+        let requestURL = MessageThreadController.baseURL.appendingPathComponent(messageThread.identifier).appendingPathComponent("messages").appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(message)
+        } catch {
+            NSLog("Error encoding message to JSON: \(error)")
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error with message thread creation data task: \(error)")
+                completion()
+                return
+            }
+            
+            completion()
+            
+        }.resume()
+    }
+    
+    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    var messageThreads: [MessageThread] = []
+}
---
+++ Message Board/Model/MessageThread 3.swift
@@ -0,0 +1,54 @@
+//
+//  MessageThread.swift
+//  Message Board
+//
+//  Created by Spencer Curtis on 8/7/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Codable, Equatable {
+
+    let title: String
+    var messages: [MessageThread.Message]
+    let identifier: String
+
+    init(title: String, messages: [MessageThread.Message] = [], identifier: String = UUID().uuidString) {
+        self.title = title
+        self.messages = messages
+        self.identifier = identifier
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let title = try container.decode(String.self, forKey: .title)
+        let identifier = try container.decode(String.self, forKey: .identifier)
+        let messages = try container.decodeIfPresent([Message].self, forKey: .messages) ?? []
+        
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+
+    
+    struct Message: Codable, Equatable {
+        
+        let messageText: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date()) {
+            self.messageText = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+    
+    static func ==(lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title &&
+            lhs.identifier == rhs.identifier &&
+            lhs.messages == rhs.messages
+    }
+}
