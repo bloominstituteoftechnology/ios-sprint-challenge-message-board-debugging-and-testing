e3f361f3dd32deb64b8f2c2e7a3f89916a0f5c78
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-21 14:21:35 -0800

    added UI Test for creating a new message

--- Message Board/bugs.md
+++ Message Board/bugs.md
@@ -8,8 +8,8 @@
 #### MessageThreadController.Swift
 * Dummy Data doesn't load during UITesting
     argument is being passed, verified by breakpoint
-* messages decode error - expected array<Any> found dictionary 
-    2020-02-21 10:10:42.326978-0800 Message Board[8483:192901] Error decoding message threads from JSON data: typeMismatch(Swift.Array<Any>, Swift.DecodingError.Context(codingPath: [_JSONKey(stringValue: "9DD6A181-045A-449E-B20A-93EE9E5ED7DD", intValue: nil), CodingKeys(stringValue: "messages", intValue: nil)], debugDescription: "Expected to decode Array<Any> but found a dictionary instead.", underlyingError: nil)) 
+    Workaround: Create Local Data in UI Tests (create message) - can then test based on that
+
 
 ***
 ### Resolved
@@ -19,6 +19,11 @@
 #### MessageThreadController.Swift
 * Line 37: Error decoding message threads from JSON data: typeMismatch(Swift.Array<Any>, Swift.DecodingError.Context(codingPath: [], debugDescription: "Expected to decode Array<Any> but found a dictionary instead.", underlyingError: nil))
     Resolution: Data comes back as [String: MessageThread] - parsed result to assign Message threads to MessageThreads array
+
+* messages decode error - expected array<Any> found dictionary 
+2020-02-21 10:10:42.326978-0800 Message Board[8483:192901] Error decoding message threads from JSON data: typeMismatch(Swift.Array<Any>, Swift.DecodingError.Context(codingPath: [_JSONKey(stringValue: "9DD6A181-045A-449E-B20A-93EE9E5ED7DD", intValue: nil), CodingKeys(stringValue: "messages", intValue: nil)], debugDescription: "Expected to decode Array<Any> but found a dictionary instead.", underlyingError: nil)) 
+Resolution: Data comes back as [String: Message] - parsed result to assign messages to messages array
+
 #### MessageDetailVC
 * Messages aren't being PUT - UI in MessageThreadDetailVC doesn't respond
     Resolution: segue identifier being checked when seguing from ThreadDetailVC was incorrect, preventing dependency injection
--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -12,8 +12,11 @@ import XCTest
 class Message_BoardUITests: XCTestCase {
     enum Identifier: String {
         case threadsCellLabel = "MessageThreadsTableViewController.CellTitleLabel"
-        case threadDetailCellTitleLabel = "MessageThreadDetailTableViewController.CellTitleLabel"
         case newThreadTextField = "ThreadTableViewController.NewThreadTextField"
+        case newMessageNameTextField = "NewMessageVC.NameTextField"
+        case newMessageNameTextArea = "NewMessageVC.NameTextArea"
+        case messageThreadCellNameLabel = "MessageThreadDetailTableViewController.CellTitleLabel"
+        case messageThreadMessageLabel = "MessageThreadDetailTableViewController.CellSubtitleLabel"
     }
     
     var app: XCUIApplication!
@@ -35,20 +38,40 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
-    func testNewMessage() {
+    func testNewThread() {
+        let app = XCUIApplication()
+        app.tables["Empty list"].textFields[Identifier.newThreadTextField.rawValue].tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         let newThreadTextField = textField(forId: .newThreadTextField)
         newThreadTextField.tap()
         newThreadTextField.typeText("Local Test")
         app.keyboards.buttons["Return"].tap()
-        
         let threadsCellLabel = label(forId: .threadsCellLabel)
         XCTAssert(threadsCellLabel.exists)
+        XCTAssertEqual(threadsCellLabel.label, "Local Test")
     }
     
-    
-    
-
-            
-            
-        
+    func testNewMessage() {
+        //create a new local thread
+        testNewThread()
+        //test and tap the first cell
+        XCTAssert(app.cells.element(boundBy: 0).isHittable)
+        app.cells.element(boundBy: 0).tap()
+        //test and tap the add button
+        let addButton = app.navigationBars.buttons["Add"]
+        XCTAssert(addButton.isHittable)
+        addButton.tap()
+        //test the name textField, type New Message, and save
+        let newMessageNameTextField = textField(forId: .newMessageNameTextField)
+        XCTAssert(newMessageNameTextField.isHittable)
+        //type text and save
+        newMessageNameTextField.tap()
+        newMessageNameTextField.typeText("I'm a new message")
+        app.navigationBars["New Message"].buttons["Send"].tap()
+        //go back and check for message
+        let backButton = app.navigationBars["New Message"].buttons["Local Test"]
+        backButton.tap() //remove if pop VC
+        XCTAssert(app.staticTexts["I'm a new message"].exists)
     }
+    
+}
