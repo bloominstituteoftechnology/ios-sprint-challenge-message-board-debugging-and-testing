d030ca0f6a04e96528065c4ca9c2e69eb3745614
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-21 16:24:41 -0800

    MVP + Stretches

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -10,17 +10,22 @@ import XCTest
 @testable import Message_Board
 
 class Message_BoardUITests: XCTestCase {
+    //=======================
+    // MARK: - Types
     enum Identifier: String {
         case threadsCellLabel = "MessageThreadsTableViewController.CellTitleLabel"
         case newThreadTextField = "ThreadTableViewController.NewThreadTextField"
         case newMessageNameTextField = "NewMessageVC.NameTextField"
-        case newMessageNameTextArea = "NewMessageVC.NameTextArea"
-        case messageThreadCellNameLabel = "MessageThreadDetailTableViewController.CellTitleLabel"
-        case messageThreadMessageLabel = "MessageThreadDetailTableViewController.CellSubtitleLabel"
     }
     
+    //=======================
+    // MARK: - Properties
     var app: XCUIApplication!
+    let newThreadTitle = "Local Test"
+    let messageText = "I'm a new message"
     
+    //=======================
+    // MARK: - Helper Methods
     func label(forId id: Identifier) -> XCUIElement {
         return app.staticTexts[id.rawValue]
     }
@@ -29,30 +34,17 @@ class Message_BoardUITests: XCTestCase {
         return app.textFields[id.rawValue]
     }
     
-    override func setUp() {
-        super.setUp()
-        app = XCUIApplication()
-        continueAfterFailure = false
-        // NOTE: Keep this setup as is for UI Testing
-        app.launchArguments = ["UITesting"]
-        app.launch()
-    }
-    
     func newThread() {
         let app = XCUIApplication()
         app.tables["Empty list"].textFields[Identifier.newThreadTextField.rawValue].tap()
         app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         let newThreadTextField = textField(forId: .newThreadTextField)
         newThreadTextField.tap()
-        let newThreadTitle = "Local Test"
         newThreadTextField.typeText(newThreadTitle)
         app.keyboards.buttons["Return"].tap()
-        let threadsCellLabel = label(forId: .threadsCellLabel)
-        XCTAssert(threadsCellLabel.exists)
-        XCTAssertEqual(threadsCellLabel.label, newThreadTitle)
     }
     
-    func testNewMessage() {
+    func newMessage() {
         //create a new local thread
         newThread()
         //test and tap the first cell
@@ -67,10 +59,30 @@ class Message_BoardUITests: XCTestCase {
         XCTAssert(newMessageNameTextField.isHittable)
         //type text and save
         newMessageNameTextField.tap()
-        let messageText = "I'm a new message"
         newMessageNameTextField.typeText(messageText)
         app.navigationBars["New Message"].buttons["Send"].tap()
-        
+    }
+    
+    //=======================
+    // MARK: - Testing
+    override func setUp() {
+        super.setUp()
+        app = XCUIApplication()
+        continueAfterFailure = false
+        // NOTE: Keep this setup as is for UI Testing
+        app.launchArguments = ["UITesting"]
+        app.launch()
+    }
+    
+    func testNewThread() {
+        newThread()
+        let threadsCellLabel = label(forId: .threadsCellLabel)
+        XCTAssert(threadsCellLabel.exists)
+        XCTAssertEqual(threadsCellLabel.label, newThreadTitle)
+    }
+    
+    func testNewMessage() {
+        newMessage()
         XCTAssert(app.staticTexts[messageText].exists)
     }
     
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,28 +10,38 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
-    func testBaseURL() {
-        XCTAssertEqual(MessageThreadController.baseURL, URL(string: "https://lambda-journal-debug-sprint.firebaseio.com/"))
-    }
     
+    //=======================
+    // MARK: - Helper Methods
     func encodeMessage() -> Data? {
         let aMessage = ["TestMessage":MessageThread.Message(text: "Hi", sender: "Tester")]
         let encodedMessage = try? JSONEncoder().encode(aMessage.self)
         return encodedMessage
     }
     
+    func decodeMessage() -> [MessageThread.Message]? {
+        let encodedMessage = encodeMessage()
+        XCTAssertNotNil(encodedMessage)
+        let decodedDummyMessageResult = try! JSONDecoder().decode([String:MessageThread.Message].self, from: encodedMessage!)
+        let decodedMessages = decodedDummyMessageResult.map { $0.value }
+        return decodedMessages
+    }
+    
+    //=======================
+    // MARK: - Testing
+    func testBaseURL() {
+        XCTAssertEqual(MessageThreadController.baseURL, URL(string: "https://lambda-journal-debug-sprint.firebaseio.com/"))
+    }
+    
     func testEncodeMessage() {
-        let aMessage = ["TestMessage":MessageThread.Message(text: "Hi", sender: "Tester")]
-        let encodedMessage = try? JSONEncoder().encode(aMessage.self)
+        let encodedMessage = encodeMessage()
         XCTAssertNotNil(encodedMessage)
     }
     
     func testDecodeMessage() {
-        let encodedMessage = encodeMessage()
-        XCTAssertNotNil(encodedMessage)
-        let decodedDummyMessageResult = try! JSONDecoder().decode([String:MessageThread.Message].self, from: encodedMessage!)
-        let decodedMessage = decodedDummyMessageResult.map { $0.value }
-        XCTAssert(decodedMessage[0].messageText == "Hi")
-        XCTAssert(decodedMessage[0].sender == "Tester")
+        let decodedMessages = decodeMessage()
+        XCTAssertNotNil(decodedMessages)
+        XCTAssert(decodedMessages![0].messageText == "Hi")
+        XCTAssert(decodedMessages![0].sender == "Tester")
     }
 }
