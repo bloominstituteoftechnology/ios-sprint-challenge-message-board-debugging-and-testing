097c2ff1c70846c6708c123907005fa8af4b3553
elizabeth paige <wingateep@Icloud.com>
2020-03-20 11:37:44 -0400

    set up a few UITests (need to fill out)

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -10,17 +10,28 @@ import XCTest
 
 class Message_BoardUITests: XCTestCase {
     
-    var app: XCUIApplication!
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
     
     override func setUp() {
         super.setUp()
         
         continueAfterFailure = false
-        app = XCUIApplication()
+        let app = XCUIApplication()
         
         // NOTE: Keep this setup as is for UI Testing
         app.launchArguments = ["UITesting"]
         app.launch()
     }
     
+    // MARK: - Tests
+    
+    func testCreateThrd() {
+        
+    }
+    
+    func testCreateMsge() {
+        
+    }
 }
