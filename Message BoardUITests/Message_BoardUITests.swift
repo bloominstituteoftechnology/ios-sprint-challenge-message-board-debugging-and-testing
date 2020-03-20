763019c9116480933a4698e1f4f1d644e1f10323
elizabeth paige <wingateep@Icloud.com>
2020-03-20 13:44:08 -0400

    Set up all Testing

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -27,11 +27,80 @@ class Message_BoardUITests: XCTestCase {
     
     // MARK: - Tests
     
-    func testCreateThrd() {
-        
+    // Test asynchronous logic using an expectation
+    func testExpectation() {
+        let didFinish = expectation(description: "didFinish")
+        var name = ""
+        let url = URL(string: "https://apple.com")!
+
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            didFinish.fulfill()
+            name = "Beth"
+        }.resume()
+
+        wait(for: [didFinish], timeout: 5) // blocking sync wait
+
+        // Assertion only happens after the time out, or web request completes
+        XCTAssertEqual("Beth", name)
     }
     
-    func testCreateMsge() {
+    func testCreateThrd() {
+        let searchTF = app.textFields["Create a new thread:"]
+        searchTF.tap()
         
+        searchTF.typeText("Testing 000 Thread")
+        app.keyboards.buttons["return"].tap()
+        
+        let newThread = app.staticTexts["Testing 000 Thread"]
+        XCTAssert(newThread.exists)
+        newThread.tap()
+        
+        let backBtn = app.navigationBars.buttons.element(boundBy: 0)
+        XCTAssert(backBtn.exists)
+        backBtn.tap()
+    }
+    
+    func testCreateMsge() { // error will try to fix later 
+        let searchTF = app.textFields["Create a new thread:"]
+         searchTF.tap()
+
+         searchTF.typeText("Testing 000 Thread")
+         app.keyboards.buttons["Return"].tap()
+
+         let newThread = app.staticTexts["Testing 000 Thread"]
+         newThread.tap()
+
+         let composeBtn = app.buttons["Add"]
+         composeBtn.tap()
+
+         let enterNameTF = app.textFields["Enter your name:"]
+         enterNameTF.tap()
+         enterNameTF.typeText("Elizabeth")
+
+         let enterMsgTV = app.textViews.element(boundBy: 0)
+         enterMsgTV.tap()
+         enterMsgTV.typeText("This is a test")
+
+         let sendBtn = app.buttons["Send"]
+         XCTAssert(sendBtn.exists)
+         sendBtn.tap()
+
+         let newMsg = app.tables.cells.staticTexts["Elizabeth"]
+         XCTAssert(newMsg.exists)
+         newMsg.tap()
+    }
+}
+
+extension XCTestCase {
+
+    func tapElementAndWaitForKeyboardToAppear(element: XCUIElement) {
+        let keyboard = XCUIApplication().keyboards.element
+        while (true) {
+            element.tap()
+            if keyboard.exists {
+                break;
+            }
+            RunLoop.current.run(until: NSDate(timeIntervalSinceNow: 0.5) as Date)
+        }
     }
 }
