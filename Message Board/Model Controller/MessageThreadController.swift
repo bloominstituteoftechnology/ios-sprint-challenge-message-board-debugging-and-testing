54a0b2d4c4f31d8a4cca18f5d1b891a1f15b1c5e
Shawn Gee <shawngee@gmail.com>
2020-04-16 16:30:39 -0400

    Changed firebase database

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://mymovies-shawngee.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
