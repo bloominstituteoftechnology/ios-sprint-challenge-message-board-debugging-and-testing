fcc66bc5ba5f9f5a4fdb799e10f7d2f73c539a1a
Shawn Gee <shawngee@gmail.com>
2020-04-16 16:31:11 -0400

    Added test for creating message thread

---
+++ MessageBoardTests/MessageThreadControllerTests.swift
@@ -0,0 +1,27 @@
+//
+//  MessageThreadControllerTests.swift
+//  MessageBoardTests
+//
+//  Created by Shawn Gee on 4/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Message_Board
+
+class MessageThreadControllerTests: XCTestCase {
+
+    func testCreateMessageThread() throws {
+        let messageThreadController = MessageThreadController()
+        
+        let expectation = XCTestExpectation(description: "Create Thread")
+        messageThreadController.createMessageThread(with: "Testing") {
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertEqual(messageThreadController.messageThreads.count, 1)
+    }
+
+}
