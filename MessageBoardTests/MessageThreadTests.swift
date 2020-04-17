1f5a6921f0bd7e932fdeba1e59283cd9a498a250
Mark Gerrior <github@gerrior.com>
2020-04-17 11:39:52 -0700

    Test case for Bug 0004

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,21 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
-    
+    func testCreateMessageThread() {
+        let controller = MessageThreadController()
+        
+        let didFinish = expectation(description: "didFinish")
+        
+        controller.createMessageThread(with: "Welcome to the Thunderdome!") {
+            didFinish.fulfill()
+        }
+        
+        wait(for: [didFinish], timeout: 5) // blocking sync wait
+
+        // Assertion only happens after the time out, or web request completes
+        // isInverted: Indicates that the expectation is not intended to happen
+        // By adding bang (!) before it, we're testing that it indeed happened!
+        XCTAssertTrue(!didFinish.isInverted, "⚠️ Writing message to the server failed.")
+    }
+
 }
