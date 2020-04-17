3824e01eb9d47a1539da6e0ca821069afb4e957f
Mark Gerrior <github@gerrior.com>
2020-04-17 16:08:51 -0700

    Added testFetchMessageThreads test

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,6 +11,7 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    // Bug 0004
     func testCreateMessageThread() {
         let controller = MessageThreadController()
         
@@ -28,4 +29,21 @@ class MessageThreadTests: XCTestCase {
         XCTAssertTrue(!didFinish.isInverted, "⚠️ Writing message to the server failed.")
     }
 
+    // Bug 0007
+    func testFetchMessageThreads() {
+        let controller = MessageThreadController()
+
+        let didFinish = expectation(description: "didFinish")
+        
+        controller.fetchMessageThreads {
+            didFinish.fulfill()
+        }
+        
+        wait(for: [didFinish], timeout: 5) // blocking sync wait
+
+        // Assertion only happens after the time out, or web request completes
+        // isInverted: Indicates that the expectation is not intended to happen
+        // By adding bang (!) before it, we're testing that it indeed happened!
+        XCTAssertTrue(!didFinish.isInverted, "⚠️ Writing message to the server failed.")
+    }
 }
