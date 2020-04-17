23ec3328a35c79b8de9ce62eb39be12a24fb6b6f
Christopher DeVito <toolofjesus@protonmail.com>
2020-04-17 13:58:06 -0400

    Added UI tests for creating a new thread aand viewing details and return. Second one is failing.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,4 +23,41 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
+    
+    // pretty sure this is not correct
+    func testCreatingThread() {
+        
+        app.tables.textFields["Create a new thread:"].tap()
+        app/*@START_MENU_TOKEN@*/.buttons["shift"]/*[[".keyboards.buttons[\"shift\"]",".buttons[\"shift\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let tKey = app/*@START_MENU_TOKEN@*/.keys["T"]/*[[".keyboards.keys[\"T\"]",".keys[\"T\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey.tap()
+        //tKey.tap()
+        
+        let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        eKey.tap()
+        //eKey.tap()
+        
+        let sKey = app/*@START_MENU_TOKEN@*/.keys["s"]/*[[".keyboards.keys[\"s\"]",".keys[\"s\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        sKey.tap()
+        //sKey.tap()
+        
+        let tKey2 = app/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey2.tap()
+        //tKey2.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        
+        
+    }
+    
+    func testViewingThreadAndReturn() {
+        testCreatingThread()
+        
+        app.tables.children(matching: .cell).element(boundBy: 1).staticTexts["Test"].tap()
+        app.navigationBars["Test"].buttons["λ Message Board"].tap()
+        
+        
+    }
+    
 }
