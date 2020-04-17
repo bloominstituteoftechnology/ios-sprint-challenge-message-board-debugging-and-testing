21378832a2e4b5c7f84009d3c5fae386e090e5cf
Hector L <hectoremil21@hotmail.com>
2020-04-17 12:16:18 -0400

    testLoadFirebase()

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,30 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    func testLoadFirebase() {
+        let didFinish = expectation(description: "didFinish")
+        var name = ""
+        let url = URL(string: "https://ios-albums-ff76b.firebaseio.com/")!
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            
+            if let data = data {
+                do {
+                    let threads = try JSONDecoder().decode([MessageThread].self, from: data)
+                    didFinish.fulfill()
+                    name = "Dave"
+            } catch {
+                
+                NSLog("Error decoding message threads from JSON data: \(error)")
+                }
+            }
+            
+        }.resume()
+        
+        wait(for: [didFinish], timeout: 5)
+        
+        XCTAssertEqual(name, "Dave")
+        
+    }
     
 }
