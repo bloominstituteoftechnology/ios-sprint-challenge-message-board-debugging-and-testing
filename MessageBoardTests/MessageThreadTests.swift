90f5442a88b686624e23a66918b10acc98044e14
Christopher DeVito <toolofjesus@protonmail.com>
2020-04-17 12:47:41 -0400

    Fixed bug where app was not decoding data froom firebase. Needed to decode a dictionary rather than an array.

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -12,4 +12,28 @@ import XCTest
 class MessageThreadTests: XCTestCase {
     
     
+    // MARK: - test will not fail
+    func testCreateThread () {
+        let messageThreadController = MessageThreadController()
+        
+        messageThreadController.createMessageThread(with: "John") {
+            XCTAssert(messageThreadController.messageThreads.count > 0)
+        }
+    }
+    
+    // MARK: - Test is successful, but not sure if this is accurate
+    func testCreateMessage() {
+        let messageThreadController = MessageThreadController()
+        
+        messageThreadController.createMessageThread(with: "John") {
+            XCTAssert(messageThreadController.messageThreads.count > 0)
+            messageThreadController.createMessage(in: messageThreadController.messageThreads[0], withText: "Test", sender: "Me") {
+                // something goes here
+            }
+        }
+    }
+    
+    
+    
+    
 }
