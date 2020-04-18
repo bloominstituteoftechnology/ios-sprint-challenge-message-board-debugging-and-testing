b2801a3fd99e1512eed6c6ea8d3bfcd1576030c2
Jessie <jessie.griffin@icloud.com>
2020-04-17 21:41:52 -0400

    Adds new firebase

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -72,7 +72,7 @@ class MessageThreadController {
             self.messageThreads.append(thread)
             completion()
             
-        }
+        }.resume() // URLSeesion was not being called becaouse .resume() was missing
     }
     
     func createMessage(in messageThread: MessageThread, withText text: String, sender: String, completion: @escaping () -> Void) {
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://messageboardsprint-fa967.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
