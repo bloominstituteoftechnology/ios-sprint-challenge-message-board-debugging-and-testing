6302eb5650f445de44769668ebd6dc88ded52801
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-04-20 00:27:22 -0400

    MVP Fixed UI Tests

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Message_Board
 
 class Message_BoardUITests: XCTestCase {
     
@@ -23,47 +24,45 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
-    // Tap existing thread in table view and verify navigation to the detail table view for that thread.
-    func testShowThreadDetail() {
-        app.tables.cells.staticTexts["Testing again"].tap()
-        XCTAssertTrue(app.navigationBars["Testing again"].exists)
-        
-    }
-    
-    func testPopViewFromNavigationStack() {
-        app.navigationBars["Testing again"].buttons["λ Message Board"].tap()
-        XCTAssertTrue(app.navigationBars["λ Message Board"].exists)
-    }
-    
     // create a new thread from the table view and verify it populated a cell.
     func testCreateMessageThread() {
-        let messageTextField = app.tables.textFields["Create a new thread:"]
-        messageTextField.tap()
-        messageTextField.typeText("I can type remotely.")
-        app.keyboards.buttons["Return"].tap()
+        newThread()
         
-        XCTAssertTrue(app.tables.staticTexts["I can type remotely."].exists)
-        
+        XCTAssertTrue(app.tables.staticTexts["Testing again"].exists)
     }
     
+    // Tap existing thread in table view and verify navigation to the detail table view for that thread.
+    func testShowThreadDetail() {
+        newThread()
+        app.tables.cells.staticTexts["Testing again"].tap()
+        XCTAssertTrue(app.navigationBars["Testing again"].exists)
+    }
+        
     // Tap on a thread to go to detail table view; tap on add button; create new message and verify it saved to the thread.
     func testCreateMessageFromThread() {
+        newThread()
         app.tables.cells.staticTexts["Testing again"].tap()
         
         app.navigationBars["Testing again"].buttons["Add"].tap()
-        
         let nameTextField = app.textFields["Enter your name:"]
         nameTextField.tap()
         nameTextField.typeText("Bob")
-        
+
+
         let messageTextView = app.textViews.element
         XCTAssert(messageTextView.exists)
         messageTextView.tap()
         messageTextView.typeText("It's working!")
-        
+
         app.navigationBars["New Message"].buttons["Send"].tap()
         
         XCTAssertTrue(app.tables.staticTexts["It's working!"].exists)
     }
     
+    func newThread() {
+        let messageTextField = app.tables.textFields["Create a new thread:"]
+        messageTextField.tap()
+        messageTextField.typeText("Testing again")
+        app.keyboards.buttons["Return"].tap()
+    }
 }
