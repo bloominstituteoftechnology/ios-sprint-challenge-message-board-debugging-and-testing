c6488d436544fba76e1ed47da9c38078d1befc8e
Jessie <jessie.griffin@icloud.com>
2020-04-20 23:43:28 -0400

    Adds Pages and Expect to extension

---
+++ Message BoardUITests/Pages/MessageThreadTableViewPage.swift
@@ -0,0 +1,9 @@
+//
+//  MessageThreadPage.swift
+//  Message BoardUITests
+//
+//  Created by Jessie Ann Griffin on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Message BoardUITests/Pages/XCTestCase+Expect.swift
@@ -0,0 +1,9 @@
+//
+//  XCTestCase+Expect.swift
+//  Message BoardUITests
+//
+//  Created by Jessie Ann Griffin on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
