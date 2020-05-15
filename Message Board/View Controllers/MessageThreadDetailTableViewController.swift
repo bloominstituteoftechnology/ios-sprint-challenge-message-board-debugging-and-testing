01c48f18afbb3a5b7b11744f93cf24fa674a254f
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-15 10:45:48 -0500

    Fixed Segue Identifier and nil messageThread

--- Message Board/View Controllers/MessageThreadDetailTableViewController.swift
+++ Message Board/View Controllers/MessageThreadDetailTableViewController.swift
@@ -14,6 +14,7 @@ class MessageThreadDetailTableViewController: UITableViewController {
         super.viewDidLoad()
 
         title = messageThread?.title
+        print("Message Thread: \(messageThread)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,11 +42,12 @@ class MessageThreadDetailTableViewController: UITableViewController {
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "AddMesage" {
+        if segue.identifier == "AddMessage" {
             guard let destinationVC = segue.destination as? MessageDetailViewController else { return }
             
             destinationVC.messageThreadController = messageThreadController
             destinationVC.messageThread = messageThread
+            print("Executing?")
         }
     }
     
