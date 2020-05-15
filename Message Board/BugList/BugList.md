2ee83e1f500a510dceef8c0963ff12a838f148f8
Brad <braddiroff@icloud.com>
2020-05-15 11:53:21 -0400

    1 bug fixing

--- Message Board/BugList/BugList.md
+++ Message Board/BugList/BugList.md
@@ -1,1 +1,3 @@
-
+1.) Problem: Does not load the data
+a.) Run the URLSession separately and see if it works as planned
+Solution: Add ".resume( )" to the end of the URLSession
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,27 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    func testTheURLSessions() {
+        
+        // This is for finding the missing .resume() call in the URLSession on func createMessageThread, it will fail because it doesn't have the .resume(), which can easily be added to make it pass
+        
+        let baseURL = URL(string: "https://sprintchallengetest.firebaseio.com/")!
+        let identifier = UUID().uuidString
+        
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
+         var request = URLRequest(url: requestURL)
+         request.httpMethod = "PUT"
+        
+        var value = ""
+        let didFinish = expectation(description: "didFinish")
+         
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            didFinish.fulfill()
+            value = "value"
+        }
+        
+        wait(for: [didFinish], timeout: 5)
+        XCTAssertEqual("value", value)
+    }
     
 }
