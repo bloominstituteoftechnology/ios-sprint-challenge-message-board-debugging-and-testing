5a3071604f170cf113a1c670bd51e53fae5267a3
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-15 11:06:00 -0500

    Added creatingThreads Unit Test

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,33 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    //Returns True If successful
+    var creatingThreads: Bool {
+        var messageThread = MessageThreadController()
+        
+        //Expectation
+        let didFinish = expectation(description: "didFinish")
+        var returnType = false
+        
+        //Networking
+        messageThread.createMessageThread(with: "Test_Message") {
+            DispatchQueue.main.async {
+                didFinish.fulfill()
+                returnType = true
+            }
+        }
+        
+        wait(for: [didFinish], timeout: 5)
+        return returnType
+    }
     
+   /* var creatingMessages: Bool {
+        
+    }*/
+    
+    
+    func testAll() {
+        XCTAssertTrue(creatingThreads)
+        //XCTAssertTrue(<#T##expression: Bool##Bool#>)
+    }
 }
