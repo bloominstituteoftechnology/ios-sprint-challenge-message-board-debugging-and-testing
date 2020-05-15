badaa7dcee7ce03fa061ac3c56dfed5d99a47020
Ezra Black <dev.ezrablack@gmail.com>
2020-05-15 16:08:05 -0500

    Added UI Testing

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -21,14 +21,48 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
-    func test() {
+    func testAddingThread() {
+        let app = XCUIApplication()
+        let _ = app.tables
+        app.textFields.element.tap()
+        app.textFields.element.typeText("test")
+        XCUIApplication().keyboards.buttons["return"].tap()
+    }
+    
+    func testOpeningThread() {
+        let app = XCUIApplication()
+        app.tables.children(matching: .cell).element(boundBy: 0).tap()
+        app.buttons["λ Message Board"].tap()
+    }
+    
+    func testUsingThread() {
         
+        let app = XCUIApplication()
+        app.textFields.element.tap()
+        app.textFields.element.typeText("test")
+        XCUIApplication().keyboards.buttons["return"].tap()
+        app.tables.children(matching: .cell).element(boundBy: 2).tap()
+        app.navigationBars["test"].buttons["Add"].tap()
+        app.textFields.element.tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+        app.navigationBars["New Message"].buttons["Send"].tap()
+    }
+    
+    func testSimpleLifeCycle() {
         let app = XCUIApplication()
         let tablesQuery = app.tables
         tablesQuery.textFields["Create a new thread:"].tap()
-        tablesQuery.children(matching: .cell).element(boundBy: 8).staticTexts["hi"].tap()
-        app.navigationBars["hi"].buttons["λ Message Board"].tap()
-        
+        app.textFields.element.typeText("new")
+        XCUIApplication().keyboards.buttons["return"].tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["new"]/*[[".cells.staticTexts[\"new\"]",".staticTexts[\"new\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.navigationBars["new"].buttons["Add"].tap()
+        let enterYourNameTextField = app.textFields["Enter your name:"]
+        enterYourNameTextField.tap()
+        enterYourNameTextField.tap()
+        let textView = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
+        textView.tap()
+        textView.tap()
+        app.navigationBars["New Message"].buttons["Send"].tap()
     }
     
 }
