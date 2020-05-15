d3cd83084d99d8eaebd7fbb9739ea6c324925bc5
Mark Poggi <markp@mac.com>
2020-05-15 09:05:50 -0700

    Switched message decoder from array to dictionary.  Created a temp container for messages dictionary.

--- Bugs.md
+++ Bugs.md
@@ -5,7 +5,7 @@ The requirements for this project are as follows:
 
 1.  Added .resume() to Message Thread Controller's createMessageThread function.
 2.  Fixed bug in MessageThreadController.  Decoder was expecting an array, but received a dictionary instead. 
-3. 
-4. 
+3.  Fixed misspelled segue identifier in MessageThreadDetailTableViewController.
+4.  Switched message decoder from array to dictionary.  Created a temp container for messages dictionary.
 5. 
 
--- Message Board/Model/MessageThread.swift
+++ Message Board/Model/MessageThread.swift
@@ -25,8 +25,12 @@ class MessageThread: Codable, Equatable {
         
         let title = try container.decode(String.self, forKey: .title)
         let identifier = try container.decode(String.self, forKey: .identifier)
-        let messages = try container.decodeIfPresent([Message].self, forKey: .messages) ?? []
-        
+        let messagesDictionary = try container.decodeIfPresent([String: Message].self, forKey: .messages)
+        var messages: [Message] = []
+        if let temp = messagesDictionary {
+            messages = Array(temp.values)
+        }
+  
         self.title = title
         self.identifier = identifier
         self.messages = messages
