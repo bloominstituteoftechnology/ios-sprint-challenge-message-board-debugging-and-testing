00398f9554e853fb7fbbb23e87d9c1ab65d25165
Chris Dobek <dobek.christopher@gmail.com>
2020-05-15 13:34:54 -0400

    Updated to Swift 5

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -7,16 +7,19 @@
 //
 
 import XCTest
+ 
 
 class Message_BoardUITests: XCTestCase {
     
-    var app: XCUIApplication!
+    private var app: XCUIApplication {
+        return XCUIApplication()
+    }
     
     override func setUp() {
         super.setUp()
         
         continueAfterFailure = false
-        app = XCUIApplication()
+        let  app = XCUIApplication()
         
         // NOTE: Keep this setup as is for UI Testing
         app.launchArguments = ["UITesting"]
@@ -24,5 +27,16 @@ class Message_BoardUITests: XCTestCase {
     }
     
     
+   func testCreatingNewThread() {
+
+       // Empty tableView
+       let threadTextField = XCUIApplication().tables.textFields["Create a new thread:"]
+       XCTAssertEqual(app.tables.cells.count, 0)
+
+       threadTextField.tap()
+       threadTextField.typeText("This is a testing thread.")
+       app.keyboards.buttons["Return"].tap()
+       XCTAssertEqual(app.tables.cells.count, 1)
+   }
     
 }
