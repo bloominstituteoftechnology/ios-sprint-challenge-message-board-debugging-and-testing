dba7094f737091f46d7bee40149f25900dce4780
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-15 11:08:06 -0400

    Added my firebase url.

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://messagethread-29299.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
