d6068e7150f147a5e57689d0eb7b6e182d23531d
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-15 10:53:44 -0500

    Testing Basic UI Functionality

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -35,6 +35,16 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
         
         
+        //Testing Basic UI Functionality
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["FOR TESTING PURPOSES DO NOT DELETE"]/*[[".cells.staticTexts[\"FOR TESTING PURPOSES DO NOT DELETE\"]",".staticTexts[\"FOR TESTING PURPOSES DO NOT DELETE\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let forTestingPurposesDoNotDeleteNavigationBar = app.navigationBars["FOR TESTING PURPOSES DO NOT DELETE"]
+        forTestingPurposesDoNotDeleteNavigationBar.buttons["Add"].tap()
+        app.navigationBars["New Message"].buttons["FOR TESTING PURPOSES DO NOT DELETE"].tap()
+        forTestingPurposesDoNotDeleteNavigationBar.buttons["λ Message Board"].tap()
+        
+        
+        
+        
     }
     
     
