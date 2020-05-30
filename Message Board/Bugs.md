58169181401558b159b0082e17f610a20ff17624
Chad Parker <chad@chadparker.com>
2020-05-29 19:33:54 -0700

    Bugfix: Send button is working now

--- Message Board/Bugs.md
+++ Message Board/Bugs.md
@@ -1,2 +1,14 @@
 #  Bugs
 
+* New Message `Send` button doesn't do anything 
+First fix: segue identifier in code did not match segue identifier in storyboard, fixed. 
+
+* `Send` button triggers a print statement but nothing happens on-screen.
+Fix: have NavigationController pop the current View Controller 
+
+
+
+
+
+
+Possible decoding error
--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,20 +23,49 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
-    // Test asynchronous logic using an expectation
-    func testExpectation() {
-        let didFinish = expectation(description: "didFinish")
-        var name = ""
-        let url = URL(string: "https://apple.com")!
+    func testNewMessageSend() {
+        
+        app.tables["Empty list"].textFields["Create a new thread:"].tap()
+        
+        app.keys["h"].tap()
+        app.keys["e"].tap()
+        app.keys["y"].tap()
+        
+        app.buttons["Return"].tap()
+        app.tables.cells.staticTexts["hey"].tap()
+        app.navigationBars["hey"].buttons["Add"].tap()
+        app.textFields["Enter your name:"].tap()
 
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
-            didFinish.fulfill()
-            name = "Dave"
-        }.resume()
-
-        wait(for: [didFinish], timeout: 5) // blocking sync wait
-
-        // Assertion only happens after the time out, or web request completes
-        XCTAssertEqual("Dave", name)
+        app.keys["c"].tap()
+        app.keys["h"].tap()
+        app.keys["a"].tap()
+        app.keys["d"].tap()
+        
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+        
+        app.keys["H"].tap()
+        app.keys["e"].tap()
+        app.keys["y"].tap()
+        
+        app.navigationBars["New Message"].buttons["Send"].tap()
+        
+        
     }
+    
+//    // Test asynchronous logic using an expectation
+//    func testExpectation() {
+//        let didFinish = expectation(description: "didFinish")
+//        var name = ""
+//        let url = URL(string: "https://apple.com")!
+//
+//        URLSession.shared.dataTask(with: url) { (data, _, _) in
+//            didFinish.fulfill()
+//            name = "Dave"
+//        }.resume()
+//
+//        wait(for: [didFinish], timeout: 5) // blocking sync wait
+//
+//        // Assertion only happens after the time out, or web request completes
+//        XCTAssertEqual("Dave", name)
+//    }
 }
