612a79fc28c20ea8feb29fd294acac83629b22ae
Claudia Maciel <clc_80@yahoo.com>
2020-05-29 18:09:22 -0700

    Added my firebase URL

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -111,6 +111,6 @@ class MessageThreadController {
         }.resume()
     }
     
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    static let baseURL = URL(string: "https://message-board-583b1.firebaseio.com/")!
     var messageThreads: [MessageThread] = []
 }
