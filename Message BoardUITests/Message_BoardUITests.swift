8180c19e47324cbb388a714c51b96d9680340011
Libby Thomas <thomaslibby05@gmail.com>
2020-05-29 22:04:55 -0500

    Successful MessageBoardTests

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,4 +23,31 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
+    // I add sleeps so that it doesn't run so fast and I can see what the test is doing.
+    func testSegueToDetailTableViewController() {
+        app.tables.staticTexts["A New Thread"].tap()
+        sleep(2)
+    }
+    
+    func testSegueToDetailViewController() {
+        app.tables.staticTexts["A New Thread"].tap()
+        app.navigationBars["A New Thread"].buttons["Add"].tap()
+    }
+    
+    func testSendingMessage() {
+        app.tables.staticTexts["A New Thread"].tap()
+        app.navigationBars["A New Thread"].buttons["Add"].tap()
+        app.textFields["Enter your name:"].tap()
+
+        let lKey = app.keys["l"]
+        lKey.tap()
+
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+        
+        
+        app.keys["T"].tap()
+
+        app.navigationBars["New Message"].buttons["Send"].tap()
+        
+    }
 }
