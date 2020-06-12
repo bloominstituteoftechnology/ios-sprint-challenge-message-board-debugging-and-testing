e2d7a905cba7798b5044dba2b28f8ba76f4631d6
Harmony Radley <harmonyradley99@gmail.com>
2020-06-12 14:17:36 -0400

    working more on this

--- Message Board/View Controllers/MessageDetailViewController.swift
+++ Message Board/View Controllers/MessageDetailViewController.swift
@@ -31,10 +31,11 @@ class MessageDetailViewController: UIViewController {
             let messageText = messageTextView.text,
             let messageThread = messageThread else { return }
         messageThreadController?.createMessage(in: messageThread, withText: messageText, sender: senderName, completion: {
-            print("Message created!")
             DispatchQueue.main.async {
                 self.navigationController?.popViewController(animated: true)
             }
+            print("Message created!")
+
         })
 
     }
