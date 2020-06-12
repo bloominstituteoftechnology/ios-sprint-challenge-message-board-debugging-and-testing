7d1a612202b4ae7b7464dc9507229f0d437f1a0f
Dahna <dbuenrostro09@gmail.com>
2020-06-12 12:18:11 -0400

    Fetching data from server working by refactoring decoding. Table View cells populating with Main queue reload.data

--- Message Board/Model/Bugs.swift
+++ Message Board/Model/Bugs.swift
@@ -15,6 +15,10 @@ import Foundation
  
  Problem: Table View still not loading
     - Check table view to make sure cells are being populated correctly
+    - Also needed to resume function and fix reload of data on main queue
+ 
+ Problem: Cancel and Send Buttons not working
+    - Check both methods to see how they are specifically designed and if they need to be refactored
     
  
  
