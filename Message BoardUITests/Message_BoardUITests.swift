305494cd4c893cba18b028e5c15c1c2f32acdb3c
Joe V <joeveverka89@gmail.com>
2020-06-12 11:45:23 -0400

    found typo in segue

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -26,9 +26,35 @@ class Message_BoardUITests: XCTestCase {
           XCTAssertEqual(app.tables.cells.count, 2)
 
           threadTextField.tap()
-          threadTextField.typeText("This is a new thread name.")
+          threadTextField.typeText("This is the name of a new thread")
           app.keyboards.buttons["Return"].tap()
           XCTAssertEqual(app.tables.cells.count, 3)
-        
     }
+
+    func testAddNewMessage() {
+
+
+        let threadTextField = XCUIApplication().tables.textFields["Create a new thread:"]
+        XCTAssertEqual(app.tables.cells.count, 0)
+
+        threadTextField.tap()
+        threadTextField.typeText("This is a new thread name.")
+        app.keyboards.buttons["Return"].tap()
+        XCTAssertEqual(app.tables.cells.count, 1)
+
+        let threadCell = XCUIApplication().tables.children(matching: .cell).element.staticTexts["This is a new thread name."]
+        threadCell.tap()
+        let navigationBar = XCUIApplication().navigationBars["This is a new thread name."].buttons["Add"]
+        navigationBar.tap()
+        let textField = XCUIApplication().textFields["Enter your name:"]
+        textField.tap()
+        textField.typeText("John Smith")
+        let addMessage = XCUIApplication().navigationBars["New Message"].buttons["Send"]
+        addMessage.tap()
+
+        // this should check that the navigation does pop and the user isn't stuck after pressing send
+        XCTAssertEqual(app.tables.cells.count, 1)
+    }
+
+
 }
