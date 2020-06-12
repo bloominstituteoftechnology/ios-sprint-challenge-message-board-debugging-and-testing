e8970e562a8b3265ae454e251b85b97fe392afcf
Dahna <dbuenrostro09@gmail.com>
2020-06-12 13:44:11 -0400

    Unit and UI Tests done, refactored unit test to use code snippet

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -10,13 +10,100 @@ import XCTest
 
 class Message_BoardUITests: XCTestCase {
     
+    // MARK: - Properties
+    var isUITesting = true
+    
     var app = XCUIApplication()
     
+    private var tables: XCUIElementQuery {
+        return app.tables
+    }
+    
+    private var enterButton: XCUIElement {
+        return app.buttons["Return"]
+    }
+    
+    private var titleTextField: XCUIElement {
+        return app.tables.textFields["Create a new thread:"]
+    }
+    
+    private var threadCell: XCUIElement {
+        return app.tables.cells["ThreadCell"]
+    }
+    
+    private var nameTextField: XCUIElement {
+        return app.textFields["Enter your name:"]
+    }
+    
+    private var messageCell: XCUIElement {
+        return app.tables.cells["MessageCell"]
+    }
+    
+    // MARK: - Setup
     override func setUpWithError() throws {
         continueAfterFailure = false
         
         // NOTE: Keep this setup as is for UI Testing
         app.launchArguments = ["UITesting"]
         app.launch()
+    }
+    
+    
+    // MARK: - Tests - The 3 tests Assert for proper UI display in order to pass
+    func testCreateNewThread() {
+        
+        titleTextField.tap()
+        titleTextField.typeText("New Thread")
+        enterButton.tap()
+        
+        XCTAssertEqual(tables.staticTexts["New Thread"].label, "New Thread")
+
+    }
+    
+    func testAddMessage() {
+
+        titleTextField.tap()
+        titleTextField.typeText("Test Add Message")
+        app.buttons["Return"].tap()
+        
+        XCTAssertEqual(tables.staticTexts["Test Add Message"].label, "Test Add Message")
+        XCTAssertEqual(threadCell.staticTexts["Test Add Message"].label, "Test Add Message")
+        let testAdd = tables.staticTexts["Test Add Message"]
+        testAdd.tap()
+        
+        let testAddNavBar = app.navigationBars["Test Add Message"]
+        testAddNavBar.buttons["Add"].tap()
+        
+        nameTextField.tap()
+        XCTAssert(nameTextField.exists)
+        nameTextField.typeText("Mark")
+        enterButton.tap()
+        
+        let messageTextView = app.textViews["MessageTextView"]
+        XCTAssert(messageTextView.exists, "MessageTextView")
+
+        messageTextView.tap()
+        messageTextView.typeText("Message")
+        
+        let newMessageNavigationBar = app.navigationBars["New Message"]
+        newMessageNavigationBar.buttons["Send"].tap()
+        XCTAssertEqual(messageCell.staticTexts["Message"].label, "Message")
+    }
+    
+    func testScreenNavigation() {
+        
+        let newThread = app.tables.staticTexts["A New Thread"]
+        newThread.tap()
+        XCTAssert(app.staticTexts["A New Thread"].exists)
+        
+        let newThreadNavBar = app.navigationBars["A New Thread"]
+        newThreadNavBar.buttons["Add"].tap()
+        XCTAssert(app.staticTexts["New Message"].exists)
+        
+        app.navigationBars["New Message"].buttons["Cancel"].tap()
+        XCTAssert(app.staticTexts["A New Thread"].exists) // This line is to check that the title is appearing now
+        
+        newThreadNavBar.buttons["λ Message Board"].tap()
+        XCTAssert(tables.textFields["Create a new thread:"].exists)
     }
 }
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -12,22 +12,50 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
-        
+    var isUITesting = false
     let mockController = MessageThreadController()
     var tableView: UITableView?
     
     
+    func expect(exists element: XCUIElement, file: String = #file, line: UInt = #line) {
+        if !element.exists {
+            recordFailure(withDescription: "Expected \(element) to exist", inFile: file, atLine: Int(line), expected: true)
+        }
+    }
+    
+    // Removing the .resume() from this function makes it fail to follow with the first bug
     func testCreateThreadOnServer() {
         
-        let title = "tester"
+        let baseURL = URL(string:"https://demomsg-e18e6.firebaseio.com/")!
+        let id = UUID().uuidString
         
-        var count = mockController.messageThreads.count
+        let requestURL = baseURL.appendingPathComponent(id).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
         
-        mockController.createMessageThread(with: title) {
-            count += 1
+        var value = ""
+        let didFinish = expectation(description: "didFinish")
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            didFinish.fulfill()
+            value = "value"
+        }.resume()
+        
+        wait(for: [didFinish], timeout: 5)
+        XCTAssertEqual("value", value)
+    }
+    
+    func testFetchDataFromServer() {
+        
+        let didFinish = expectation(description: "didFinish")
+        
+        var messageThreads: [MessageThread] = []
+        
+        mockController.fetchMessageThreads {
+            didFinish.fulfill()
+            messageThreads = self.mockController.messageThreads
         }
-        XCTAssertEqual(count, mockController.messageThreads.count)
-        
-        
+        wait(for: [didFinish], timeout: 10)
+        XCTAssertNotNil(messageThreads)
     }
 }
