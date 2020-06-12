bd27f430e88b044a7d31b2f39a1c9365576a5327
Harmony Radley <harmonyradley99@gmail.com>
2020-06-12 11:12:52 -0400

    Created Bugs file and created own firebase link

---
+++ Message Board/Model/Bugs.swift
@@ -0,0 +1,21 @@
+//
+//  Bugs.swift
+//  Message Board
+//
+//  Created by Harmony Radley on 6/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+
+ 1.
+ 2.
+ 3.
+ 4.
+ 5.
+ 
+
+ */
