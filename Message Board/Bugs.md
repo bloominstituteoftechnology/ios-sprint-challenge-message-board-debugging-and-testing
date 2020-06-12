12e67dc753f4c421eb4ed53ee7b16a6b1c58e24d
Kelson Hartle <kelson99hartle@icloud.com>
2020-06-12 11:51:58 -0600

    almost everything done except tests

---
+++ Message Board/Bugs.md
@@ -0,0 +1,18 @@
+//BUGS.md
+
+/// BUG: Table view does not update when adding a new thread nor does the array update with the new value,
+/// Fixed by putting the data task inside of a do block and resume the task at the end of the do block and placing the tableView.ReloadData() on the main thread.
+
+///BUG: Detail view does not update with thread title as the view controller's title
+
+
+///BUG: Table view controller does not pull down data from firebase and update the tableview.
+/// Fixed by making the required coding keys and changing the decoding strategy from an array to a dictionary.
+
+
+///BUG: Detail View button was unable to be cancelled and modal view was not dissmissing.
+///Fixed by added a self.dissmiss method to the IBAction
+
+///BUG: View Controller does not dissmiss after clicking save on the send message modal view controller
+
+///BUG: Unable to create a new message to send to the thread.
--- Message Board/View Controllers/MessageThreadDetailTableViewController.swift
+++ Message Board/View Controllers/MessageThreadDetailTableViewController.swift
@@ -11,14 +11,19 @@ import UIKit
 class MessageThreadDetailTableViewController: UITableViewController {
     
     // MARK: - Properties
-
+    
     var messageThread: MessageThread?
     var messageThreadController: MessageThreadController?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = messageThread?.title
+        
+        updateViews()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,15 +32,20 @@ class MessageThreadDetailTableViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
+    func updateViews() {
+        guard let messageThread = messageThread else { return }
+        
+        title = messageThread.title
+    }
     // MARK: - UITableViewDataSource
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messageThread?.messages.count ?? 0
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
-
+        
         let message = messageThread?.messages[indexPath.row]
         
         cell.textLabel?.text = message?.text
@@ -43,15 +53,17 @@ class MessageThreadDetailTableViewController: UITableViewController {
         
         return cell
     }
-
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddMesage" {
-            guard let destinationVC = segue.destination as? MessageDetailViewController else { return }
             
-            destinationVC.messageThreadController = messageThreadController
-            destinationVC.messageThread = messageThread
+            if let navC = segue.destination as? UINavigationController,
+                let destinationVC = navC.viewControllers.first as? MessageDetailViewController {
+                destinationVC.messageThread = messageThread
+//                destinationVC.messageThreadController = messageThreadController
+            }
         }
     }
 }
