1e3f54764036aafea26f31fb218fe03537848f73
nonye <nnezekwo@gmail.com>
2020-06-12 11:13:01 -0700

    Can't get passed the save button

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -34,7 +34,7 @@ class MessageThreadController {
             guard let data = data else { NSLog("No data returned from data task"); completion(); return }
             do {
                #warning ("not decoding - debug error #3.")
-                self.messageThreads = try Array(JSONDecoder().decode([String : MessageThread].self, from: data).values)
+                self.messageThreads = try JSONDecoder().decode([String : MessageThread].self, from: data).map({ $0.value })
             } catch {
                 self.messageThreads = []
                 NSLog("Error decoding message threads from JSON data: \(error)")
