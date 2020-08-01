cc9cdffb97645ca882092c5d13604a2e2ba349a1
Libby Thomas <thomaslibby05@gmail.com>
2020-07-31 20:37:01 -0500

    MVP
    Successful UI Tests and cleaned up some code.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,4 +23,47 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
+    func testSeguetoDetailTableVC() {
+        tapANewThread()
+        sleep(2)
+    }
+    
+    func testSegueToDetailVC() {
+        tapANewThread()
+        tapAddButton()
+        sleep(2)
+    }
+    
+    func testSendingMessage() {
+        tapANewThread()
+        tapAddButton()
+        app.textFields["Enter your name:"].tap()
+
+        let lKey = app.keys["l"]
+        lKey.tap()
+        
+        tapMessageTextView()
+
+        let tKey = app.keys["t"]
+        tKey.tap()
+        
+        app.navigationBars["New Message"].buttons["Send"].tap()
+        
+    }
+    
+    
+    // MARK: - Functions
+    
+    func tapANewThread() {
+        app.tables.staticTexts["A New Thread"].tap()
+    }
+    
+    func tapAddButton() {
+        app.navigationBars["A New Thread"].buttons["Add"].tap()
+    }
+    
+    func tapMessageTextView() {
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+    }
+    
 }
