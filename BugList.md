6b988596597ff2819f960f6d947ed28830d46e85
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-31 18:13:07 -0700

    Fixed issue with thread not being created

--- BugList.md
+++ BugList.md
@@ -1,10 +1,17 @@
 #  Bug List
 
 ## Fixed Issues:
+- Fixed issue with Creating Thread. Network call not being initiated
+* Added .resume() to network call when creating a message thread
+* Need to confirm fix with unit test
+* Confirmed Fix with unit test [func testCreatingThreadAndSendingToServer()]
+
 
 
 
 
 ## Known Issues:
+- Can't create message thread 
+* [FIXED SEE ABOVE]
 
-
+- 
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,16 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    // MARK: - Properties
+    let controller = MessageThreadController()
+    
+    // MARK: - Tests
+    // GOAL: - Create a thread and pass to server, check to see if the thread exists
+    func testCreatingThreadAndSendToServer(){
+        let messageThread = MessageThread(title: "Test")
+        controller.createMessageThread(with: messageThread.title) {
+            XCTAssertTrue(self.controller.messageThreads[0].title == "Test")
+        }
+    }
     
 }
