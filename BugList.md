6d5897bfb54d39163aa8a2f1319974435fe039a6
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-31 18:21:53 -0700

    Fixed issue with Fetch data task not being initalized

--- BugList.md
+++ BugList.md
@@ -6,7 +6,11 @@
 * Need to confirm fix with unit test
 * Confirmed Fix with unit test [func testCreatingThreadAndSendingToServer()]
 
+- Fixed issue with app crashing when creating thread
+* Wrapped tableView.reloadData() inside of the main thread
 
+- Fixed issue when trying to fetch data from server. Network call not being initiated
+* Added .resume() to network call when fetching message thread
 
 
 
@@ -14,4 +18,8 @@
 - Can't create message thread 
 * [FIXED SEE ABOVE]
 
-- 
+- App crashes when tableView is reloaded
+* [FIXED SEE ABOVE]
+
+- Error Fetching from server
+* [FIXED SEE ABOVE]
--- Message Board/View Controllers/MessageThreadDetailTableViewController.swift
+++ Message Board/View Controllers/MessageThreadDetailTableViewController.swift
@@ -11,31 +11,32 @@ import UIKit
 class MessageThreadDetailTableViewController: UITableViewController {
     
     // MARK: - Properties
-
+    
     var messageThread: MessageThread?
     var messageThreadController: MessageThreadController?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = messageThread?.title
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        self.tableView.reloadData()
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     // MARK: - UITableViewDataSource
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messageThread?.messages.count ?? 0
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
-
+        
         let message = messageThread?.messages[indexPath.row]
         
         cell.textLabel?.text = message?.text
@@ -43,9 +44,9 @@ class MessageThreadDetailTableViewController: UITableViewController {
         
         return cell
     }
-
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddMesage" {
             guard let destinationVC = segue.destination as? MessageDetailViewController else { return }
