e74f02207e6b1d1790cff1973f5f306af1d57a82
Libby Thomas <thomaslibby05@gmail.com>
2020-07-31 20:05:12 -0500

    Successful MessageThreadTests

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -10,6 +10,24 @@ import XCTest
 @testable import Message_Board
 
 class MessageThreadTests: XCTestCase {
+   
+    var app: XCUIApplication!
     
+    func testCreateThread() {
+        let controller = MessageThreadController()
+        controller.createMessageThread(with: "UITest Thread") {
+            XCTAssert(controller.messageThreads.count > 0)
+        }
+    }
+    
+    func testCreateMessage() {
+        let controller = MessageThreadController()
+        controller.createMessageThread(with: "UITest Thread") {
+            XCTAssert(controller.messageThreads.count > 0)
+            controller.createMessage(in: controller.messageThreads[0], withText: "Testing", sender: "Someone") {
+                sleep(2)
+            }
+        }
+    }
     
 }
