fb41d79554f2fc9be228fadc8e43b716b456aaf5
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-07-31 23:04:07 -0500

    Working on UITests.

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Message_Board
 
 class Message_BoardUITests: XCTestCase {
     
@@ -19,4 +20,32 @@ class Message_BoardUITests: XCTestCase {
         app.launchArguments = ["UITesting"]
         app.launch()
     }
+
+    func testMessageBoardTableView() {
+        app.launchArguments = ["UITesting"]
+        app.launch()
+
+        let tableView = app.tables.firstMatch
+
+        XCTAssert(tableView.exists)
+    }
+
+    func testTitleDisplays() {
+        app.navigationBars["λ Message Board"].staticTexts["λ Message Board"].tap()
+
+        XCTAssert(app.navigationBars["λ Message Board"].exists)
+    }
+
+    func testTableViewText() {
+
+
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["MessageTest"]/*[[".cells.matching(identifier: \"CreatedMessages\").staticTexts[\"MessageTest\"]",".staticTexts[\"MessageTest\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Test Message"]/*[[".cells[\"MessageText\"].staticTexts[\"Test Message\"]",".staticTexts[\"Test Message\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Sender: Josh"]/*[[".cells[\"MessageText\"].staticTexts[\"Sender: Josh\"]",".staticTexts[\"Sender: Josh\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+
+        XCTAssert(tablesQuery.staticTexts["MessageTest"].exists)
+    }
+
 }
