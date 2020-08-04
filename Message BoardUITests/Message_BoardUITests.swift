43f3e976d97d0bc616f9a8c4b80b11f691ea40a4
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-08-04 00:15:01 -0500

    Added some more tests

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -39,13 +39,55 @@ class Message_BoardUITests: XCTestCase {
     func testTableViewText() {
 
 
-        let tablesQuery = XCUIApplication().tables
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["MessageTest"]/*[[".cells.matching(identifier: \"CreatedMessages\").staticTexts[\"MessageTest\"]",".staticTexts[\"MessageTest\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Test Message"]/*[[".cells[\"MessageText\"].staticTexts[\"Test Message\"]",".staticTexts[\"Test Message\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Sender: Josh"]/*[[".cells[\"MessageText\"].staticTexts[\"Sender: Josh\"]",".staticTexts[\"Sender: Josh\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["A New Thread"].tap()
+        tablesQuery.staticTexts["Checking to make sure this works."].tap()
+        tablesQuery.staticTexts["Joe"].tap()
         
 
-        XCTAssert(tablesQuery.staticTexts["MessageTest"].exists)
+        XCTAssert(tablesQuery.staticTexts["Checking to make sure this works."].exists)
     }
 
+    func testCreateNewMessage() {
+
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["A New Thread"].tap()
+        app.navigationBars["A New Thread"].buttons["Add"].tap()
+
+        let newmessagesenderTextField = app/*@START_MENU_TOKEN@*/.textFields["NewMessageSender"]/*[[".textFields[\"Enter your name:\"]",".textFields[\"NewMessageSender\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        newmessagesenderTextField.tap()
+        app.textViews["NewMessageText"].tap()
+
+        let hKey = app/*@START_MENU_TOKEN@*/.keys["H"]/*[[".keyboards.keys[\"H\"]",".keys[\"H\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        hKey.tap()
+
+
+        let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        eKey.tap()
+
+
+        let lKey = app/*@START_MENU_TOKEN@*/.keys["l"]/*[[".keyboards.keys[\"l\"]",".keys[\"l\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        lKey.tap()
+        lKey.tap()
+
+        let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        oKey.tap()
+
+        newmessagesenderTextField.tap()
+
+        let jKey = app/*@START_MENU_TOKEN@*/.keys["j"]/*[[".keyboards.keys[\"j\"]",".keys[\"j\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        jKey.tap()
+        oKey.tap()
+
+        let hKey2 = app/*@START_MENU_TOKEN@*/.keys["h"]/*[[".keyboards.keys[\"h\"]",".keys[\"h\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        hKey2.tap()
+
+        let nKey = app/*@START_MENU_TOKEN@*/.keys["n"]/*[[".keyboards.keys[\"n\"]",".keys[\"n\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        nKey.tap()
+
+        app.navigationBars["New Message"].buttons["Send"].tap()
+        sleep(3)
+
+        XCTAssert(app.navigationBars["A New Thread"].exists)
+    }
 }
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -12,10 +12,22 @@ import XCTest
 class MessageThreadTests: XCTestCase {
     let messageThreadController = MessageThreadController()
 
+
     func testCreateMessage() {
         messageThreadController.createLocalMessageThread(with: "Hello") {
             return
         }
         XCTAssertEqual(1, self.messageThreadController.messageThreads.count)
+    }
+
+    func testSendMessage() {
+        let messageThreadMessage = MessageThread.Message(text: "Hello there.", sender: "Josh", timestamp: Date())
+        let messageTread = MessageThread(title: "Hi There", messages: [messageThreadMessage], identifier: "FCAB7137-1D84-40F5-94A7-8931032DAF87")
+
+        messageThreadController.createMessage(in: messageTread, withText: "Hello there.", sender: "Josh") {
+            return
+        }
+
+
     }
 }
