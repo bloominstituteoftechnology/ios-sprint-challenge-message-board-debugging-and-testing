7a506c46569bb0d08b637e5cbfa9590764e3bd88
Robert Vance <Fearmydotzs@gmail.com>
2020-09-25 19:43:14 -0600

    Corrected spelling in prepare for segue function for AddMessage.

--- Message Board/View Controllers/MessageThreadsTableViewController.swift
+++ Message Board/View Controllers/MessageThreadsTableViewController.swift
@@ -35,7 +35,10 @@ class MessageThreadsTableViewController: UITableViewController {
         threadTitleTextField.text = ""
         
         messageThreadController.createMessageThread(with: threadTitle) {
-            self.tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                
+            }
         }
     }
     
