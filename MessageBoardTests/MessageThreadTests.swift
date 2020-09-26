6fe536946ca36c86b2f6b88452e2d296c17dfee8
NorlanT <ntibanear@gmail.com>
2020-09-26 13:32:25 -0400

    unitTest

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,33 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    var messageThreadController = MessageThreadController()
+   
+    // Creating a new Thread
+    func testNewThread() {
+        let messageThread = MessageThread(title: "Testing")
+        messageThreadController.createMessageThread(with: messageThread.title) {
+            XCTAssertTrue(self.messageThreadController.messageThreads[0].title == "Testing")
+        }
+        
+    }
     
-}
+    func testLoadMessage() {
+        XCTAssertNotNil(messageThreadController.messageThreads)
+    }
+    
+    
+    func testcreateMessage() {
+        let messageTitle = MessageThread(title: "Sprint Challenge")
+        messageThreadController.createMessageThread(with: messageTitle.title) {
+            self.messageThreadController.createMessage(in: messageTitle, withText: "This one is confusing", sender: "NTL") {
+                XCTAssertTrue(self.messageThreadController.messageThreads[0].messages[0].sender == "NTL")
+            }
+        }
+        
+        
+        
+        
+    }
+    
+}//
