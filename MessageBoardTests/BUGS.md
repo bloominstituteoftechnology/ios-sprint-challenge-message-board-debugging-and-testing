a58e2f36271301009cd226b0e647274b2eead3cf
Ben James <bejjmail@gmail.com>
2020-09-27 19:16:08 -0700

    bug 1 completed

--- MessageBoardTests/BUGS.md
+++ MessageBoardTests/BUGS.md
@@ -1,4 +1,6 @@
 #  BUGS
 
-
+1. missing .resume( ) at end of `URLSession.shared.dataTask`
+    a. added .resume( ) on line after closure on line 71 of MessageThread Controller
+    b. Created test and verified failed and passed after implimenting fix.
 
--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,34 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    func testCreatingNewThread() {
+        let messageThreadController = MessageThreadController()
+        createMessageThread(withTitle: "New Thread Title", using: messageThreadController)
+    }
     
+}
+
+extension MessageThreadTests {
+    
+    private func createMessageThread(withTitle threadTitle: String, using controller: MessageThreadController) {
+        let existingMessageThreadCount = controller.messageThreads.count
+        let expectation = self.expectation(description: "New Thread Title")
+        
+        controller.createMessageThread(with: threadTitle) {
+            expectation.fulfill()
+        }
+        
+        assertExpectation(expectation)
+        
+        XCTAssertEqual(controller.messageThreads.count, existingMessageThreadCount + 1)
+        XCTAssertTrue(controller.messageThreads.contains(where: { $0.title == threadTitle }))
+    }
+    
+    private func assertExpectation(_ expectation: XCTestExpectation, timeout: Double = 5.0) {
+        waitForExpectations(timeout: timeout) { error in
+            if let error = error {
+                XCTAssert(false, "\(error)")
+            }
+        }
+    }
 }
