a27859e16ca23dc90e877121c25738e46008f2e0
Kenneth Jones <kennethjones1991@outlook.com>
2020-09-28 21:26:14 -0600

    Attempted to add expectations to unit tests, but it didn't work out; also added decoding test

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,6 +11,42 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
+    // I tried to add expectations but either I did it wrong or my tests just fail now. I left the tests as I first had them below these commented ones.
+    
+//    func testCreateNewThread() {
+//        let messageThreadController = MessageThreadController()
+//        let didFinish = expectation(description: "didFinish")
+//
+//        XCTAssertEqual(messageThreadController.messageThreads.count, 0)
+//        messageThreadController.createMessageThread(with: "Thread title yo") {
+//            didFinish.fulfill()
+//            self.wait(for: [didFinish], timeout: 5)
+//
+//            XCTAssertEqual(messageThreadController.messageThreads.count, 1)
+//            XCTAssertEqual(messageThreadController.messageThreads[0].title, "Thread title yo")
+//        }
+//    }
+//
+//    func testCreateNewMessage() {
+//        let mTC = MessageThreadController()
+//        let didFinish = expectation(description: "didFinish")
+//
+//        XCTAssertEqual(mTC.messageThreads.count, 0)
+//        mTC.createMessageThread(with: "Thread title yo") {
+//            didFinish.fulfill()
+//            self.wait(for: [didFinish], timeout: 5)
+//
+//            XCTAssertEqual(mTC.messageThreads.count, 1)
+//            XCTAssertEqual(mTC.messageThreads[0].title, "Thread title yo")
+//            XCTAssertEqual(mTC.messageThreads[0].messages.count, 0)
+//            mTC.createMessage(in: mTC.messageThreads[0], withText: "Hey what up?", sender: "Me") {
+//                XCTAssertEqual(mTC.messageThreads[0].messages.count, 1)
+//                XCTAssertEqual(mTC.messageThreads[0].messages[0].text, "Hey what up?")
+//                XCTAssertEqual(mTC.messageThreads[0].messages[0].sender, "Me")
+//            }
+//        }
+//    }
+    
     func testCreateNewThread() {
         let messageThreadController = MessageThreadController()
         XCTAssertEqual(messageThreadController.messageThreads.count, 0)
@@ -35,6 +71,19 @@ class MessageThreadTests: XCTestCase {
         }
     }
     
+    func testDecoding() throws {
+        let mTC = MessageThreadController()
+        do {
+            let data = try Data(contentsOf: mTC.mockDataURL)
+            let threadsToDecode = Array(try JSONDecoder().decode([String: MessageThread].self, from: data).values)
+            XCTAssertNoThrow(data)
+            XCTAssertNoThrow(threadsToDecode)
+        } catch {
+            XCTAssertTrue(false)
+        }
+    }
+    
+    // Left this here for reference when trying to add expectation testing to above unit tests.
     func testExpectation() {
         let didFinish = expectation(description: "didFinish")
         var name = ""
